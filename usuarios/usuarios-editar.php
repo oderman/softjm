0a152c9446c646dc2b9ab5074a281eaf3f36dced
Enuar Lara <enuar2110@gmail.com>
2022-10-26 12:59:17 -0500

    Se limpia codigo y se actualizan consultas

--- usuarios/usuarios-editar.php
+++ usuarios/usuarios-editar.php
@@ -1,37 +1,16 @@
-<?php include("sesion.php");?>
-<?php
+<?php 
+include("sesion.php");
+
 $idPagina = 4;
-$paginaActual['pag_nombre'] = "Editar Usuarios";
-?>
-<?php include("verificar-paginas.php");?>
-<?php include("head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
-?>
-<?php
-$resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios 
+
+include("verificar-paginas.php");
+include("head.php");
+$consuluta=$conexionBdPrincipal->query("SELECT * FROM usuarios 
 INNER JOIN usuarios_tipos ON utipo_id=usr_tipo
-WHERE usr_id='".$_GET["id"]."'",$conexion));
+WHERE usr_id='".$_GET["id"]."'");
+$resultadoD = mysqli_fetch_array($consuluta, MYSQLI_BOTH);
 ?>
-<!-- styles -->
-
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
-
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
-
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui-1.10.1.custom.min.js"></script>
@@ -159,14 +138,6 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 				<div class="span12">
 					<div class="primary-head">
 						<h3 class="page-header"><?=$paginaActual['pag_nombre'];?></h3>
-						
-                        <ul class="top-right-toolbar">
-							<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-							</li>
-							<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-							<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-						</ul>
-                        
 					</div>
 					<ul class="breadcrumb">
 						<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -198,14 +169,13 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 							
 							<img src="files/fotos/<?=$resultadoD['usr_foto'];?>" width="100">
 
-							<form class="form-horizontal" method="post" action="sql.php" enctype="multipart/form-data">
-                            <input type="hidden" name="idSql" value="2">
+							<form class="form-horizontal" method="post" action="bd_update/actualizar-usuarios.php" enctype="multipart/form-data">
                             <input type="hidden" name="id" value="<?=$_GET["id"];?>">
 
                             <div class="control-group">
                                         <label class="control-label">Foto de perfil</label>
                                         <div class="controls">
-                                            <input type="file" class="span4" name="foto">
+                                            <input type="file" class="span4" name="foto" multiple>
                                         </div>
                                     </div>
                             	
@@ -236,8 +206,8 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="tipoU">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM usuarios_tipos",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = $conexionBdPrincipal->query("SELECT * FROM usuarios_tipos");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 											?>
                                             	<option value="<?=$resOp[0];?>" <?php if($resultadoD['usr_tipo']==$resOp[0]) echo "selected";?>><?=$resOp[1];?></option>
                                             <?php
@@ -253,8 +223,8 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="sucursal">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM sucursales_propias",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = $conexionBdPrincipal->query("SELECT * FROM sucursales_propias");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 											?>
                                             	<option value="<?=$resOp[0];?>" <?php if($resultadoD['usr_sucursal']==$resOp[0]) echo "selected";?>><?=$resOp[1];?></option>
                                             <?php
@@ -282,8 +252,8 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="area">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM areas",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = $conexionBdPrincipal->query("SELECT * FROM areas");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 											?>
                                             	<option value="<?=$resOp[0];?>" <?php if($resultadoD['usr_area']==$resOp[0]) echo "selected";?>><?=$resOp[1];?></option>
                                             <?php
@@ -301,8 +271,8 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="ciudad">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM localidad_ciudades INNER JOIN localidad_departamentos ON dep_id=ciu_departamento ORDER BY ciu_nombre",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = $conexionBdPrincipal->query("SELECT * FROM localidad_ciudades INNER JOIN localidad_departamentos ON dep_id=ciu_departamento ORDER BY ciu_nombre");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 											?>
                                             	<option value="<?=$resOp['ciu_id'];?>" <?php if($resultadoD['usr_ciudad']==$resOp['ciu_id']){echo "selected";}?>><?=$resOp['ciu_nombre'].", ".$resOp['dep_nombre'];?></option>
                                             <?php
@@ -351,8 +321,7 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 							<h3> Cambiar contraseña</h3>
 						</div>
 						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="sql.php">
-                            <input type="hidden" name="idSql" value="75">
+							<form class="form-horizontal" method="post" action="bd_update/actualizar-claves-usuarios.php">
                             <input type="hidden" name="id" value="<?=$_GET["id"];?>">
                                 
                                 
