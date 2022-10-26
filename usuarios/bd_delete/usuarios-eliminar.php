fca4329f4a5a6c84a6a6dbfa23a5f45b343e3466
Enuar Lara <enuar2110@gmail.com>
2022-10-26 13:19:57 -0500

    Se crea archivo eliminar usuarios

---
+++ usuarios/bd_delete/usuarios-eliminar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("../sesion.php");
+
+	$idPagina = 53;
+    include(RUTA_PROYECTO."/usuarios/verificar-paginas.php");
+
+	$conexionBdPrincipal->query("DELETE FROM usuarios WHERE usr_id='" . $_GET["id"] . "'");
+
+    include(RUTA_PROYECTO."/usuarios/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="' . $_SERVER['HTTP_REFERER'] . '";</script>';
+	exit();
