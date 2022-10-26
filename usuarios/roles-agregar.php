0c3a1a58b32304303471c9bcf94672782de88140
Enuar Lara <enuar2110@gmail.com>
2022-10-26 15:31:08 -0500

    Se actualizan consultas y se limpia codigo

--- usuarios/roles-agregar.php
+++ usuarios/roles-agregar.php
@@ -1,31 +1,11 @@
-<?php include("sesion.php");?>
-<?php
+<?php include("sesion.php");
+
 $idPagina = 6;
-$paginaActual['pag_nombre'] = "Agregar Roles";
-?>
-<?php include("verificar-paginas.php");?>
-<?php include("head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
-?>
-<!-- styles -->
 
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
+include("verificar-paginas.php");
+include("head.php");
+?>
 <link href="css/chosen.css" rel="stylesheet">
-
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
 
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
@@ -146,23 +126,12 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 <body>
 <div class="layout">
 	<?php include("encabezado.php");?>
-    
-    
-    
 	<div class="main-wrapper">
 		<div class="container-fluid">
 			<div class="row-fluid ">
 				<div class="span12">
 					<div class="primary-head">
 						<h3 class="page-header"><?=$paginaActual['pag_nombre'];?></h3>
-						
-                        <ul class="top-right-toolbar">
-							<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-							</li>
-							<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-							<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-						</ul>
-                        
 					</div>
 					<ul class="breadcrumb">
 						<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -178,8 +147,7 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 							<h3> <?=$paginaActual['pag_nombre'];?></h3>
 						</div>
 						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="sql.php">
-                            <input type="hidden" name="idSql" value="3">
+							<form class="form-horizontal" method="post" action="bd_create/roles-guardar.php">
                                 
                                 <div class="control-group">
 									<label class="control-label">Nombre</label>
@@ -194,8 +162,8 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 										<select data-placeholder="Escoja una opción" class="chzn-select span6" multiple tabindex="4" name="paginasP[]">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM ".$bdAdmin.".paginas",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = $conexionBdAdmin->query("SELECT * FROM ".$bdAdmin.".paginas");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 											?>
                                             	<option value="<?=$resOp[0];?>"><?="[".$resOp[0]."] ".$resOp[1];?></option>
                                             <?php }?>
