c53eb36f370d0f01c7c271f38e8ceba0835b68ee
Enuar Lara <enuar2110@gmail.com>
2022-10-26 16:21:47 -0500

    Se limpia codigo y se actualizan consultas

--- usuarios/roles-editar.php
+++ usuarios/roles-editar.php
@@ -1,35 +1,14 @@
-<?php include("sesion.php");?>
-<?php
+<?php include("sesion.php");
+
 $idPagina = 7;
-$paginaActual['pag_nombre'] = "Editar Roles";
-?>
-<?php include("verificar-paginas.php");?>
-<?php include("head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
-?>
-<?php
-$resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios_tipos WHERE utipo_id='".$_GET["id"]."'",$conexion));
+include("verificar-paginas.php");
+include("head.php");
+
+$consulta=$conexionBdPrincipal->query("SELECT * FROM usuarios_tipos WHERE utipo_id='".$_GET["id"]."'");
+$resultadoD = mysqli_fetch_array($consulta, MYSQLI_BOTH);
 ?>
 <!-- styles -->
-
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
-
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
-
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui-1.10.1.custom.min.js"></script>
@@ -162,14 +141,6 @@ $resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios_tipos WHERE 
 				<div class="span12">
 					<div class="primary-head">
 						<h3 class="page-header"><?=$paginaActual['pag_nombre'];?></h3>
-						
-                        <ul class="top-right-toolbar">
-							<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-							</li>
-							<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-							<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-						</ul>
-                        
 					</div>
 					<ul class="breadcrumb">
 						<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -181,91 +152,14 @@ $resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios_tipos WHERE 
             <p><a href="roles-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a> <div id="solo"></div></p>
             
             <?php include("notificaciones.php");?>
-
-
-            <!--
-			<div class="row-fluid">
-				<div class="span12">
-					<div class="content-widgets gray">
-						<div class="widget-head bondi-blue">
-							<h3> <?=$paginaActual['pag_nombre'];?></h3>
-						</div>
-                        
-						
-                        
-                        
-                        
-						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="#sql.php" name="roles" id="roles">
-                            <input type="hidden" name="idSql" value="4">
-                            <input type="hidden" name="id" value="<?=$_GET["id"];?>">
-                                
-                                <div class="control-group">
-									<label class="control-label">Nombre</label>
-									<div class="controls">
-										<input type="text" class="span4" name="nombre" id="nombre" value="<?=$resultadoD['utipo_nombre'];?>">
-									</div>
-								</div> 
-                                
-                                <div class="control-group">
-									<label class="control-label">Paginas NO permitidas</label>
-									<div class="controls">
-										<select data-placeholder="Escoja una opción" class="chzn-select span6" multiple tabindex="4" name="paginasNP[]">
-											<option value=""></option>
-                                            <?php
-											$conOp = mysql_query("SELECT * FROM paginas",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
-												$paginasNP = mysql_num_rows(mysql_query("SELECT * FROM paginas_perfiles WHERE pper_pagina='".$resOp[0]."' AND pper_tipo_usuario='".$resultadoD['utipo_id']."'",$conexion));
-											?>
-                                            	<option value="<?=$resOp[0];?>"><?=$resOp[1];?></option>
-                                            <?php }?>
-										</select>
-									</div>
-								</div>
-                                
-                                <span style="color:#F00;">Utiliza la siguiente opción (<b>Acciones NO permitidas</b>) sólo si NO utilizaste la opción anterior (<b>Paginas NO permitidas</b>) y viceversa.</span>
-                                
-                                <div class="control-group">
-									<label class="control-label">Acciones NO permitidas</label>
-									<div class="controls">
-										<select data-placeholder="Escoja una opción" class="chzn-select span6" multiple tabindex="4" name="accionesNP[]">
-											<option value=""></option>
-                                            <option value="2">Agregar</option>
-                                            <option value="3">Editar</option>
-                                            <option value="4">Eliminar</option>
-										</select>
-									</div>
-								</div>
-                               
-								<div class="form-actions">
-									<a href="javascript:history.go(-1);" class="btn btn-primary"><i class="icon-arrow-left"></i> Regresar</a>
-                                    <button type="submit" class="btn btn-info"><i class="icon-save"></i> Guardar cambios</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-                
-			</div>
-			-->
-                                            
-
-
-
             <div class="row-fluid">
 				<div class="span12">
 					<div class="content-widgets gray">
 						<div class="widget-head bondi-blue">
 							<h3> <?=$paginaActual['pag_nombre'];?> (NUEVO)</h3>
 						</div>
-                        
-						
-                        
-                        
-                        
 						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="sql.php" name="roles" id="roles">
-                            <input type="hidden" name="idSql" value="4">
+							<form class="form-horizontal" method="post" action="bd_update/actualizar-roles.php" name="roles" id="roles">
                             <input type="hidden" name="id" value="<?=$_GET["id"];?>">
                                 
                                 <div class="control-group">
@@ -281,9 +175,11 @@ $resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios_tipos WHERE 
 										<select data-placeholder="Escoja una opción" class="chzn-select span6" multiple tabindex="4" name="paginasP[]">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM ".$bdAdmin.".paginas",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
-												$paginasP = mysql_num_rows(mysql_query("SELECT * FROM paginas_perfiles WHERE pper_pagina='".$resOp[0]."' AND pper_tipo_usuario='".$resultadoD['utipo_id']."'",$conexion));
+											$conOp = $conexionBdAdmin->query("SELECT * FROM ".$bdAdmin.".paginas");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
+
+                                                $consultaPagPerfiles=$conexionBdPrincipal->query("SELECT * FROM paginas_perfiles WHERE pper_pagina='".$resOp[0]."' AND pper_tipo_usuario='".$resultadoD['utipo_id']."'");
+												$paginasP = $consultaPagPerfiles->num_rows;
 											?>
                                             	<option <?php if($paginasP>0){echo "selected";}?> value="<?=$resOp[0];?>"><?="[".$resOp[0]."] ".$resOp[1];?></option>
                                             <?php }?>
