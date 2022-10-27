b9ad603edd6d32f2da9828a8610fe6605fbd6d31
Enuar Lara <enuar2110@gmail.com>
2022-10-27 09:21:38 -0500

    Se crea Archivo para eliminar paginas

---
+++ usuarios/bd_delete/paginas-eliminar.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("../sesion.php");
+
+	$idPagina = 180;
+    include(RUTA_PROYECTO."/usuarios/verificar-paginas.php");
+
+	$conexionBdAdmin->query("DELETE FROM paginas WHERE pag_id='" . $_GET["id"] . "'");
+
+    include(RUTA_PROYECTO."/usuarios/guardar-historial-acciones.php");
+
+	echo '<script type="text/javascript">window.location.href="../paginas.php?msg=3";</script>';
+	exit();
