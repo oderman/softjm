9a307ab4f72c64f1119be07f9155c8422bb7cbe0
Enuar Lara <enuar2110@gmail.com>
2022-10-27 11:08:50 -0500

    Se limita consultas clientes

--- usuarios/usuarios-editar-zonas.php
+++ usuarios/usuarios-editar-zonas.php
@@ -1,37 +1,17 @@
-<?php include("sesion.php");?>
-<?php
+<?php include("sesion.php");
+
 $idPagina = 176;
-$paginaActual['pag_nombre'] = "Editar Zonas de usuario";
-?>
-<?php include("verificar-paginas.php");?>
-<?php include("head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
-?>
-<?php
-$resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM usuarios 
+
+include("verificar-paginas.php");
+include("head.php");
+
+$consuluta=$conexionBdPrincipal->query("SELECT * FROM usuarios 
 INNER JOIN usuarios_tipos ON utipo_id=usr_tipo
-WHERE usr_id='".$_GET["id"]."'",$conexion));
+WHERE usr_id='".$_GET["id"]."'");
+$resultadoD = mysqli_fetch_array($consuluta, MYSQLI_BOTH);
 ?>
 <!-- styles -->
-
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
-
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
-
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui-1.10.1.custom.min.js"></script>
@@ -159,14 +139,6 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 				<div class="span12">
 					<div class="primary-head">
 						<h3 class="page-header"><?=$paginaActual['pag_nombre'];?></h3>
-						
-                        <ul class="top-right-toolbar">
-							<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-							</li>
-							<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-							<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-						</ul>
-                        
 					</div>
 					<ul class="breadcrumb">
 						<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -175,10 +147,6 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 					</ul>
 				</div>
 			</div>
-
-
-            
-            <!--<p><a href="usuarios-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a></p>-->
             
             <?php include("notificaciones.php");?>
             
@@ -231,9 +199,10 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="zona[]" multiple>
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM zonas",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
-												$numZ = mysql_num_rows(mysql_query("SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$resultadoD['usr_id']."' AND zpu_zona='".$resOp['zon_id']."'",$conexion));
+											$conOp = $conexionBdPrincipal->query("SELECT * FROM zonas");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
+                                                $consultaZonasUser=$conexionBdPrincipal->query("SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$resultadoD['usr_id']."' AND zpu_zona='".$resOp['zon_id']."'");
+												$numZ = $consultaZonasUser->num_rows;
 											?>
                                             	<option value="<?=$resOp['zon_id'];?>" <?php if($numZ>0){echo "selected";}?>><?=$resOp['zon_nombre'];?></option>
                                             <?php
@@ -250,10 +219,11 @@ WHERE usr_id='".$_GET["id"]."'",$conexion));
 										<select data-placeholder="Escoja una opción..." class="chzn-select span8" tabindex="2" name="cliente[]" multiple>
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT cli_id, cli_nombre FROM clientes",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
-
-												$numZ = mysql_num_rows(mysql_query("SELECT cliu_cliente, cliu_usuario FROM clientes_usuarios WHERE cliu_usuario='".$resultadoD['usr_id']."' AND cliu_cliente='".$resOp[0]."'",$conexion));
+                                            
+											$conOp = $conexionBdPrincipal->query("SELECT cli_id, cli_nombre FROM clientes  LIMIT 20");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
+                                                $consultaCliente=$conexionBdPrincipal->query("SELECT cliu_cliente, cliu_usuario FROM clientes_usuarios WHERE cliu_usuario='".$resultadoD['usr_id']."' AND cliu_cliente='".$resOp[0]."'");
+												$numZ = $consultaCliente->num_rows;
 											?>
                                             	<option value="<?=$resOp['cli_id'];?>" <?php if($numZ>0){echo "selected";}?>><?=$resOp['cli_nombre'];?></option>
                                             <?php
