2cc1405eeb1e949e6f7967cd8e609fbed3525f70
Enuar Lara <enuar2110@gmail.com>
2022-10-28 16:53:09 -0500

    Se reemplaza escript para formularios por include

--- usuarios/servicios-editar.php
+++ usuarios/servicios-editar.php
@@ -45,104 +45,10 @@ $resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM servicios WHERE serv_
 <script src="js/custom.js"></script>
 <script src="js/respond.min.js"></script>
 <script src="js/ios-orientationchange-fix.js"></script>
-<script type="text/javascript">
-    /*====TAGS INPUT====*/
-    $(function () {
-        $('#tags_1').tagsInput({
-            width: 'auto'
-        });
-        $('#tags_2').tagsInput({
-            width: 'auto',
-            onChange: function (elem, elem_tags) {
-                var languages = ['php', 'ruby', 'javascript'];
-                $('.tag', elem_tags).each(function () {
-                    if ($(this).text().search(new RegExp('\\b(' + languages.join('|') + ')\\b')) >= 0) $(this).css('background-color', 'yellow');
-                });
-            }
-        });
-    });
-    /*====Select Box====*/
-    $(function () {
-        $(".chzn-select").chosen();
-        $(".chzn-select-deselect").chosen({
-            allow_single_deselect: true
-        });
-    });
-    /*====Color Picker====*/
-    $(function () {
-        $('.colorpicker').colorpicker({
-            format: 'hex'
-        });
-        $('.pick-color').colorpicker();
-    });
-    /*====DATE Time Picker====*/
-    $(function () {
-        $('#datetimepicker1').datetimepicker({
-            language: 'pt-BR'
-        });
-    });
-    $(function () {
-        $('#datetimepicker3').datetimepicker({
-            pickDate: false
-        });
-    });
-    $(function () {
-        $('#datetimepicker4').datetimepicker({
-            pickTime: false
-        });
-    });
-    /*DATE RANGE PICKER*/
-    $(function () {
-        $('#reportrange').daterangepicker({
-            ranges: {
-                'Today': ['today', 'today'],
-                'Yesterday': ['yesterday', 'yesterday'],
-                'Last 7 Days': [Date.today().add({
-                    days: -6
-                }), 'today'],
-                'Last 30 Days': [Date.today().add({
-                    days: -29
-                }), 'today'],
-                'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
-                'Last Month': [Date.today().moveToFirstDayOfMonth().add({
-                    months: -1
-                }), Date.today().moveToFirstDayOfMonth().add({
-                    days: -1
-                })]
-            },
-            opens: 'left',
-            format: 'MM/dd/yyyy',
-            separator: ' to ',
-            startDate: Date.today().add({
-                days: -29
-            }),
-            endDate: Date.today(),
-            minDate: '01/01/2012',
-            maxDate: '12/31/2013',
-            locale: {
-                applyLabel: 'Submit',
-                fromLabel: 'From',
-                toLabel: 'To',
-                customRangeLabel: 'Custom Range',
-                daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-                monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                firstDay: 1
-            },
-            showWeekNumbers: true,
-            buttonClasses: ['btn-danger']
-        },
-        function (start, end) {
-            $('#reportrange span').html(start.toString('MMMM d, yyyy') + ' - ' + end.toString('MMMM d, yyyy'));
-        });
-        //Set the initial state of the picker label
-        $('#reportrange span').html(Date.today().add({
-            days: -29
-        }).toString('MMMM d, yyyy') + ' - ' + Date.today().toString('MMMM d, yyyy'));
-    });
-    $(function () {
-        $('#reservation').daterangepicker();
-    });
-</script>
+<?php 
+//Son todas las funciones javascript para que los campos del formulario funcionen bien.
+include("includes/js-formularios.php");
+?>
 <?php include("includes/texto-editor.php");?>
 </head>
 <body>
