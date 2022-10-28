c29c23e903b12157883b26458d2aca59cd44ad70
Jhon Oderman <jhooderman@gmail.com>
2022-10-28 15:54:54 -0500

    Impedir que se eliminen modulos y/o paginas.

--- usuarios/bd_delete/modulos-eliminar.php
+++ usuarios/bd_delete/modulos-eliminar.php
@@ -4,9 +4,12 @@
 	$idPagina = 186;
     include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
 
-	$conexionBdAdmin->query("DELETE FROM modulos WHERE mod_id='" . $_GET["id"] . "'");
+	include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
 
-    include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+	echo "Por seguridad NO es posible eliminar estos registros.";
+	exit();
+
+	$conexionBdAdmin->query("DELETE FROM modulos WHERE mod_id='" . $_GET["id"] . "'");
 
 	echo '<script type="text/javascript">window.location.href="../modulos.php?msg=3";</script>';
 	exit();
--- usuarios/bd_delete/paginas-eliminar.php
+++ usuarios/bd_delete/paginas-eliminar.php
@@ -4,9 +4,12 @@
 	$idPagina = 180;
     include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
 
-	$conexionBdAdmin->query("DELETE FROM paginas WHERE pag_id='" . $_GET["id"] . "'");
+	include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
 
-    include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+	echo "Por seguridad NO es posible eliminar estos registros.";
+	exit();
+
+	$conexionBdAdmin->query("DELETE FROM paginas WHERE pag_id='" . $_GET["id"] . "'");
 
 	echo '<script type="text/javascript">window.location.href="../paginas.php?msg=3";</script>';
 	exit();
