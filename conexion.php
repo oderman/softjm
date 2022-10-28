e3a5aecf6edb658d570e00b79056cfb46ac64bed
Jhon Oderman <jhooderman@gmail.com>
2022-10-28 15:55:54 -0500

    Nos conectamos al servidor y añadimos BD de store.

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-const SERVER = 'localhost' ;
-const USER = 'root';
-const PASS = '';
-const MAINBD = 'odermancom_jm_crm';
+const SERVER = 'orioncrm.com.co' ;
+const USER = 'orioncrmcom_dev';
+const PASS = 'Y7)octcBq]#5';
+const MAINBD = 'orioncrmcom_dev_jm_crm';
 
 $conexionBdPrincipal = new mysqli(SERVER, USER, PASS, MAINBD);
-$conexionBdAdmin = new mysqli(SERVER, USER, PASS, 'orioncrmcom_crm_admin');
+$conexionBdAdmin = new mysqli(SERVER, USER, PASS, 'orioncrmcom_dev_crm_admin');
+$conexionBdStore = new mysqli(SERVER, USER, PASS, 'orioncrmcom_dev_crm_store');
 
 const REDIRECT_ROUTE = 'http://localhost/works-projects/softjm/';
