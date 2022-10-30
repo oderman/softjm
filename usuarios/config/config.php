3763ea651cbc896097fa6b2a122e5df7566cf162
Jhon Oderman <jhooderman@gmail.com>
2022-10-30 15:19:05 -0500

    Borrar consulta duplicada.

--- usuarios/config/config.php
+++ usuarios/config/config.php
@@ -1,8 +1,4 @@
 <?php
-$consultaConfig = $conexionBdPrincipal->query("SELECT * FROM configuracion WHERE conf_id=1");
-$configu = mysqli_fetch_array($consultaConfig, MYSQLI_BOTH);
-
-//Estaba en la barra lateral
 $consultaConfig2 = $conexionBdPrincipal->query("SELECT * FROM configuracion WHERE conf_id=1");
 $configuracion = mysqli_fetch_array($consultaConfig2, MYSQLI_BOTH);
 
