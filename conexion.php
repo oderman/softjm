eef67fd54634d87d8bafd14720e0f12b3935ff2d
Jhon Oderman <jhooderman@gmail.com>
2022-10-30 17:31:27 -0500

    Crear CONSTANTE bdadmin

--- conexion.php
+++ conexion.php
@@ -3,9 +3,10 @@ const SERVER = 'orioncrm.com.co' ;
 const USER = 'orioncrmcom_dev';
 const PASS = 'Y7)octcBq]#5';
 const MAINBD = 'orioncrmcom_dev_jm_crm';
+const BDADMIN = 'orioncrmcom_dev_crm_admin';
 
 $conexionBdPrincipal = new mysqli(SERVER, USER, PASS, MAINBD);
-$conexionBdAdmin = new mysqli(SERVER, USER, PASS, 'orioncrmcom_dev_crm_admin');
+$conexionBdAdmin = new mysqli(SERVER, USER, PASS, BDADMIN);
 $conexionBdStore = new mysqli(SERVER, USER, PASS, 'orioncrmcom_dev_crm_store');
 
 const REDIRECT_ROUTE = 'http://localhost/works-projects/softjm/';
