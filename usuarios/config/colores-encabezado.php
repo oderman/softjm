b7cdbf2f10e1611e731db4d6b8c0bec556b2d9aa
Jhon Oderman <jhooderman@gmail.com>
2022-10-30 15:18:34 -0500

    Crear pagina para setear colores del encabezado.

---
+++ usuarios/config/colores-encabezado.php
@@ -0,0 +1,35 @@
+
+<?php 
+//Consulta para traer los colores de BD.
+?>
+
+<style type="text/css">
+.navbar-inverse .navbar-inner {
+	background-color: #2C3E50;
+}
+
+.navbar-inverse .brand, .navbar-inverse .nav > li > a {
+	color: #FFF;
+}
+
+.navbar-inverse .brand, .navbar-inverse .nav > li > a:hover {
+	color: #B3B6B7;
+}
+
+.navbar-inverse .nav li.dropdown.open > .dropdown-toggle, .navbar-inverse .nav li.dropdown.active > .dropdown-toggle, .navbar-inverse .nav li.dropdown.open.active > .dropdown-toggle {
+	background-color: #5D6D7E;
+	color: #FFF;
+}
+
+.dropdown-menu {
+	background-color: gray;
+}
+
+.top-nav .nav .dropdown-menu ul {
+	background-color: #2C3E50;
+}
+
+.dropdown-menu li > a:hover, .dropdown-menu li > a:focus, .dropdown-submenu:hover > a {
+	background-color: orange;
+}
+</style>
