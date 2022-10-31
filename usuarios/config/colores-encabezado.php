82a3b72f4a9258e4e91c32d61322c7318a6c69f2
Enuar Lara <enuar2110@gmail.com>
2022-10-31 14:13:03 -0500

    Correccion de nombrado de campos en consultas

--- usuarios/config/colores-encabezado.php
+++ usuarios/config/colores-encabezado.php
@@ -6,31 +6,31 @@
 
 <style type="text/css">
 .navbar-inverse .navbar-inner {
-	background-color: <?=$colorEncabezado[1];?>;
+	background-color: <?=$colorEncabezado['cxe_fondo'];?>;
 }
 
 .navbar-inverse .brand, .navbar-inverse .nav > li > a {
-	color: <?=$colorEncabezado[2];?>;
+	color: <?=$colorEncabezado['cxe_text_items'];?>;
 }
 
 .navbar-inverse .brand, .navbar-inverse .nav > li > a:hover {
-	color: <?=$colorEncabezado[3];?>;
+	color: <?=$colorEncabezado['cxe_text_items_hover'];?>;
 }
 
 .navbar-inverse .nav li.dropdown.open > .dropdown-toggle, .navbar-inverse .nav li.dropdown.active > .dropdown-toggle, .navbar-inverse .nav li.dropdown.open.active > .dropdown-toggle {
-	background-color: <?=$colorEncabezado[4];?>;
-	color: <?=$colorEncabezado[5];?>;
+	background-color: <?=$colorEncabezado['cxe_fondo_items_activo'];?>;
+	color: <?=$colorEncabezado['cxe_text_items_activo'];?>;
 }
 
 .dropdown-menu {
-	background-color: <?=$colorEncabezado[6];?>;
+	background-color: <?=$colorEncabezado['cxe_border_submenu'];?>;
 }
 
 .top-nav .nav .dropdown-menu ul {
-	background-color: <?=$colorEncabezado[7];?>;
+	background-color: <?=$colorEncabezado['cxe_fondo_submenu'];?>;
 }
 
 .dropdown-menu li > a:hover, .dropdown-menu li > a:focus, .dropdown-submenu:hover > a {
-	background-color: <?=$colorEncabezado[8];?>;
+	background-color: <?=$colorEncabezado['cxe_fondo_submenu_hover'];?>;
 }
 </style>
