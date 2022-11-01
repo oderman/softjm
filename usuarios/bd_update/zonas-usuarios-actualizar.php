13da9c62ab0e890243a43b1c7fca33d9bb507a67
Enuar Lara <enuar2110@gmail.com>
2022-11-01 11:27:17 -0500

    Correccion al actualizar datos en base de datos

--- usuarios/bd_update/zonas-usuarios-actualizar.php
+++ usuarios/bd_update/zonas-usuarios-actualizar.php
@@ -9,7 +9,6 @@ if($_POST['zonasTodas'] == 1){
 
     $conexionBdPrincipal->query("DELETE FROM zonas_usuarios WHERE zpu_usuario='" . $_POST["id"] . "'");
 
-
     $zonasConsulta = $conexionBdPrincipal->query("SELECT * FROM zonas",$conexion);
     while($zonasDatos = mysqli_fetch_array($zonasConsulta, MYSQLI_BOTH)){
 
@@ -32,15 +31,19 @@ if($_POST['zonasTodas'] == 1){
 
 
 //asignar clientes
-$numero = (count($_POST["cliente"]));
-$contador = 0;
-$conexionBdPrincipal->query("DELETE FROM clientes_usuarios WHERE cliu_usuario='" . $_POST["id"] . "'");
-while ($contador < $numero) {
-        $conexionBdPrincipal->query("INSERT INTO clientes_usuarios(cliu_usuario, cliu_cliente, cliu_fecha)VALUES('" . $_POST["id"] . "'," . $_POST["cliente"][$contador] . ", now())");
-        $contador++;
+if(isset($_POST["cliente"])){
+    $numero = (count($_POST["cliente"]));
+    $contador = 0;
+    $conexionBdPrincipal->query("DELETE FROM clientes_usuarios WHERE cliu_usuario='" . $_POST["id"] . "'");
+    while ($contador < $numero) {
+            $conexionBdPrincipal->query("INSERT INTO clientes_usuarios(cliu_usuario, cliu_cliente, cliu_fecha)VALUES('" . $_POST["id"] . "'," . $_POST["cliente"][$contador] . ", now())");
+            $contador++;
+    }
+}else{
+    $conexionBdPrincipal->query("DELETE FROM clientes_usuarios WHERE cliu_usuario='" . $_POST["id"] . "'");
 }
 
 include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
 
-echo '<script type="text/javascript">window.location.href="usuarios-editar-zonas.php?id=' . $_POST["id"] . '&msg=2";</script>';
+echo '<script type="text/javascript">window.location.href="../usuarios-editar-zonas.php?id=' . $_POST["id"] . '&msg=2";</script>';
 exit();
