9c5af9f0f08e9d08c447907ec664063ed3112984
Enuar Lara <enuar2110@gmail.com>
2022-11-01 07:46:14 -0500

    Se crea pagina independiente para guardar marcas

---
+++ usuarios/bd_create/marcas-guardar.php
@@ -0,0 +1,14 @@
+<?php
+    require_once("../sesion.php");
+
+    $idPagina = 192;
+    include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
+
+    $conexionBdPrincipal->query("INSERT INTO marcas(mar_nombre)VALUES('" . $_POST["nombre"] . "')");
+
+    $idInsertU = mysqli_insert_id($conexionBdPrincipal);
+
+    include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+
+    echo '<script type="text/javascript">window.location.href="../marcas-editar.php?id=' . $idInsertU . '&msg=1";</script>';
+    exit();
