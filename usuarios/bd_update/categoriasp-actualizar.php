159dee42671f795973e58adce5729c532fd98773
Enuar Lara <enuar2110@gmail.com>
2022-11-01 12:01:43 -0500

    Se crea pagina independiente editar categorias BD

---
+++ usuarios/bd_update/categoriasp-actualizar.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("../sesion.php");
+
+    $idPagina = 198;
+
+    include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
+
+	$conexionBdPrincipal->query("UPDATE productos_categorias SET catp_nombre='" . $_POST["nombre"] . "' WHERE catp_id='" . $_POST["id"] . "'");
+
+    include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
+
+	echo '<script type="text/javascript">window.location.href="../categoriasp-editar.php?id=' . $_POST["id"] . '&msg=2";</script>';
+	exit();
