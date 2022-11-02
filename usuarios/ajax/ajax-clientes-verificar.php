4f2ac2368bb792c3e7c874f2da163e58074c8e26
Enuar Lara <enuar2110@gmail.com>
2022-11-02 11:57:43 -0500

    Se actualizan consultas y se organiza codigo

--- usuarios/ajax/ajax-clientes-verificar.php
+++ usuarios/ajax/ajax-clientes-verificar.php
@@ -1,10 +1,10 @@
 <?php
-include("sesion.php");
-$configuracion = mysql_fetch_array(mysql_query("SELECT * FROM configuracion WHERE conf_id=1",$conexion));
+include("../sesion.php");
 
 if($_POST["opcion"]==1){
-	$clienteV = mysql_fetch_array(mysql_query("SELECT * FROM clientes WHERE cli_usuario='".trim($_POST["usuario"])."'",$conexion));
-	if($clienteV[0]!=""){
+	$consulta=$conexionBdPrincipal->query("SELECT * FROM clientes WHERE cli_usuario='".trim($_POST["usuario"])."'");
+	$clienteV = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+	if(isset($clienteV[0])){
 		echo "<span style='font-family:arial; text-align:center; color:red;'>Ya existe un cliente con este n&uacute;mero de NIT: <b><a href='clientes-editar.php?id=".$clienteV['cli_id']."'>".$clienteV['cli_nombre']."</a></b></div>";
 		exit();
 	}else{
@@ -14,9 +14,10 @@ if($_POST["opcion"]==1){
 }
 
 if($_POST["opcion"]==2){
-	$clienteV = mysql_fetch_array(mysql_query("SELECT * FROM proveedores WHERE prov_documento='".trim($_POST["usuario"])."'",$conexion));
-	if($clienteV[0]!=""){
-		echo "<span style='font-family:arial; text-align:center; color:red;'>Ya existe un proveedores con este DNI: <b><a href='proveedores-editar.php?id=".$clienteV['prov_id']."'>".$clienteV['prov_nombre']."</a></b></div>";
+	$consulta=$conexionBdPrincipal->query("SELECT * FROM proveedores WHERE prov_documento='".trim($_POST["usuario"])."'");
+	$proveedoresV = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+	if(isset($proveedoresV[0])){
+		echo "<span style='font-family:arial; text-align:center; color:red;'>Ya existe un proveedores con este DNI: <b><a href='proveedores-editar.php?id=".$proveedoresV['prov_id']."'>".$proveedoresV['prov_nombre']."</a></b></div>";
 		exit();
 	}else{
 		echo "<span style='font-family:arial; text-align:center; color:blue;'>DNI disponible, puedes continuar.</div>";
@@ -25,8 +26,9 @@ if($_POST["opcion"]==2){
 }
 
 if($_POST["opcion"]==3){
-	$datos = mysql_fetch_array(mysql_query("SELECT * FROM productos WHERE prod_referencia='".trim($_POST["idUnico"])."'",$conexion));
-	if($datos[0]!=""){
+	$consulta=$conexionBdPrincipal->query("SELECT * FROM productos WHERE prod_referencia='".trim($_POST["idUnico"])."'");
+	$datos = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+	if(isset($datos[0])){
 		echo "<span style='font-family:arial; text-align:center; color:red;'>Ya existe un registro con esta Referencia: <b><a href='productos-editar.php?id=".$datos[0]."'>".$datos['prod_nombre']."</a></b></div>";
 		exit();
 	}else{
