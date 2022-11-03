02df01a4d53b469029ddb30b5ef6b7ac8455be3f
Enuar Lara <enuar2110@gmail.com>
2022-11-03 10:40:55 -0500

    Se actualizan Consultas

--- usuarios/bodegas-productos-agregar.php
+++ usuarios/bodegas-productos-agregar.php
@@ -1,35 +1,13 @@
-<?php include("sesion.php"); ?>
 <?php
+include("sesion.php");
+
 $idPagina = 146;
-$paginaActual['pag_nombre'] = "Agregar productos a bodegas";
-?>
-<?php include("includes/verificar-paginas.php"); ?>
-<?php include("includes/head.php"); ?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('" . $_SESSION["id"] . "', '" . $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING'] . "', '" . $idPagina . "', now(),'" . $_SERVER['HTTP_REFERER'] . "')", $conexion);
-if (mysql_errno() != 0) {
-    echo mysql_error();
-    exit();
-}
+
+include("includes/verificar-paginas.php");
+include("includes/head.php");
 ?>
 <!-- styles -->
-
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
-
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
-
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
 <script src="js/jquery-ui-1.10.1.custom.min.js"></script>
@@ -179,14 +157,6 @@ if (mysql_errno() != 0) {
                     <div class="span12">
                         <div class="primary-head">
                             <h3 class="page-header"><?= $paginaActual['pag_nombre']; ?></h3>
-
-                            <ul class="top-right-toolbar">
-                                <li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-                                </li>
-                                <li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-                                <li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-                            </ul>
-
                         </div>
                         <ul class="breadcrumb">
                             <li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -202,8 +172,7 @@ if (mysql_errno() != 0) {
                                 <h3> <?= $paginaActual['pag_nombre']; ?></h3>
                             </div>
                             <div class="widget-container">
-                                <form class="form-horizontal" method="post" action="sql.php">
-                                    <input type="hidden" name="idSql" value="80">
+                                <form class="form-horizontal" method="post" action="bd_create/productos-bodegas-guardar.php">
 
                                     <div class="control-group">
                                         <label class="control-label">Producto (*)</label>
@@ -211,10 +180,10 @@ if (mysql_errno() != 0) {
                                             <select data-placeholder="Escoja una opción..." class="chzn-select span8" tabindex="2" name="producto" required>
                                                 <option value=""></option>
                                                 <?php
-                                                $conOp = mysql_query("SELECT * FROM productos", $conexion);
-                                                while ($resOp = mysql_fetch_array($conOp)) {
+                                                $conOp = $conexionBdPrincipal->query("SELECT * FROM productos");
+                                                while ($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)) {
                                                 ?>
-                                                    <option value="<?= $resOp[0]; ?>" <?php if($resOp[0] == $_GET["prod"]){echo "selected";} ?> ><?= $resOp[1]; ?></option>
+                                                    <option value="<?= $resOp[0]; ?>" <?php if(isset($_GET["prod"])){if($resOp[0] == $_GET["prod"]){echo "selected";}} ?> ><?= $resOp[1]; ?></option>
                                                 <?php
                                                 }
                                                 ?>
@@ -228,10 +197,10 @@ if (mysql_errno() != 0) {
                                             <select data-placeholder="Escoja una opción..." class="chzn-select span8" tabindex="2" name="bodega" required>
                                                 <option value=""></option>
                                                 <?php
-                                                $conOp = mysql_query("SELECT * FROM bodegas", $conexion);
-                                                while ($resOp = mysql_fetch_array($conOp)) {
+                                                $conOp = $conexionBdPrincipal->query("SELECT * FROM bodegas", $conexion);
+                                                while ($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)) {
                                                 ?>
-                                                    <option value="<?= $resOp[0]; ?>" <?php if($resOp[0] == $_GET["bod"]){echo "selected";} ?> ><?= $resOp[1]; ?></option>
+                                                    <option value="<?= $resOp[0]; ?>" <?php if(isset($_GET["bod"])){if($resOp[0] == $_GET["bod"]){echo "selected";}} ?> ><?= $resOp[1]; ?></option>
                                                 <?php
                                                 }
                                                 ?>
@@ -242,7 +211,7 @@ if (mysql_errno() != 0) {
                                     <div class="control-group">
                                         <label class="control-label">Existencia (*)</label>
                                         <div class="controls">
-                                            <input type="text" class="span2" name="existencia" value="<?=$_GET["ex"];?>" required>
+                                            <input type="text" class="span2" name="existencia" value="<?php if(isset($_GET["ex"])){$_GET["ex"];}?>" required>
                                         </div>
                                     </div>
 
