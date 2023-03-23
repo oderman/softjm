b7baa5a448ee30ac7905da9632babb8537ea76eb
Enuar Lara <enuar2110@gmail.com>
2023-03-22 19:43:29 -0500

    Se cambia variable y se añade arror reporting

--- constantes.php
+++ constantes.php
@@ -1,12 +1,14 @@
 <?php
 switch($_SERVER['HTTP_HOST']){
 	case 'localhost':
-        define('RUTA_PROYECTO', 'C:/xampp/htdocs/works-projects/softjm');
-        define('REDIRECT_ROUTE', 'http://localhost/works-projects/softjm');
+        define('RUTA_PROYECTO', 'C:/xampp/htdocs/softjm');
+        define('REDIRECT_ROUTE', 'http://localhost/softjm');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 
 	case 'nuevo.orioncrm.com.co':
         define('RUTA_PROYECTO', '/home4/orioncrmcom/public_html/nuevo.orioncrm.com.co/softjmgit');
         define('REDIRECT_ROUTE', 'https://nuevo.orioncrm.com.co/softjmgit');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 }
