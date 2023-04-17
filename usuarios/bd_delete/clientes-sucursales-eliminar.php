52f78f183352ef58b21b552000fcf84e27418900
Enuar Lara <enuar2110@gmail.com>
2023-04-17 12:05:51 -0500

    Se manda id del cliente al eliminar sucursal

--- usuarios/bd_delete/clientes-sucursales-eliminar.php
+++ usuarios/bd_delete/clientes-sucursales-eliminar.php
@@ -5,5 +5,5 @@ $idPagina = 86;
 include("includes/verificar-paginas.php");
 mysqli_query($conexionBdPrincipal,"DELETE FROM sucursales WHERE sucu_id='" . $_GET["id"] . "'");
 
-echo '<script type="text/javascript">window.location.href="../clientes-sucursales.php";</script>';
+echo '<script type="text/javascript">window.location.href="../clientes-sucursales.php?cte='.$_GET["cte"].'";</script>';
 exit();
