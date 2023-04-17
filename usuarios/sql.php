f3213a519445ae3e5741cc5a37fc93c8fb2526ae
Enuar Lara <enuar2110@gmail.com>
2023-04-17 11:38:20 -0500

    Se crea archivo independiente para actualizar sucursal en BD

--- usuarios/sql.php
+++ usuarios/sql.php
@@ -862,14 +862,9 @@ if ($_POST["idSql"] == 34) {
 
 
 //AGREGAR SUCURSALES
-	
+
 //EDITAR SUCURSALES
-if ($_POST["idSql"] == 38) {
-	mysqli_query($conexionBdPrincipal,"UPDATE sucursales SET sucu_ciudad='" . $_POST["ciudad"] . "', sucu_direccion='" . $_POST["direccion"] . "', sucu_telefono='" . $_POST["telefono"] . "', sucu_celular='" . $_POST["celular"] . "', sucu_telefonos='" . $_POST["telefonos"] . "', sucu_nombre='" . $_POST["nombre"] . "' WHERE sucu_id='" . $_POST["id"] . "'");
-	
-	echo '<script type="text/javascript">window.location.href="clientes-sucursales-editar.php?id=' . $_POST["id"] . '&msg=1&cte=' . $_POST["cte"] . '";</script>';
-	exit();
-}
+
 //AGREGAR TIKCETS CLIENTES
 if ($_POST["idSql"] == 39) {
 	$sucursal = $_POST["sucursal"];
