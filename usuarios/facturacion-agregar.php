edd5852c66fb606f664359065eca3075484b1a25
Enuar Lara <enuar2110@gmail.com>
2023-04-24 14:41:02 -0500

    Se actualizan consultas para la facturacion

--- usuarios/facturacion-agregar.php
+++ usuarios/facturacion-agregar.php
@@ -1,31 +1,17 @@
-<?php include("sesion.php");?>
-<?php
+<?php 
+include("sesion.php");
+
 $idPagina = 21;
 $paginaActual['pag_nombre'] = "Agregar producto al cliente";
+
+include("includes/verificar-paginas.php");
+include("includes/head.php");
 ?>
-<?php include("includes/verificar-paginas.php");?>
-<?php include("includes/head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
-?>
+
 <!-- styles -->
 
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
 
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
 
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
@@ -62,14 +48,6 @@ include("includes/js-formularios.php");
 				<div class="span12">
 					<div class="primary-head">
 						<h3 class="page-header"><?=$paginaActual['pag_nombre'];?></h3>
-						
-                        <ul class="top-right-toolbar">
-							<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-							</li>
-							<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-							<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-						</ul>
-                        
 					</div>
 					<ul class="breadcrumb">
 						<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -85,8 +63,8 @@ include("includes/js-formularios.php");
 							<h3> <?=$paginaActual['pag_nombre'];?></h3>
 						</div>
 						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="sql.php" enctype="multipart/form-data">
-                            <input type="hidden" name="idSql" value="11">
+							<form class="form-horizontal" method="post" action="bd_create/facturacion-guardar.php" enctype="multipart/form-data">
+                            <input type="hidden" name="cte" value="<?=$_GET["cte"];?>">
 
                                 
                                 <div class="control-group">
@@ -103,10 +81,11 @@ include("includes/js-formularios.php");
 										<select data-placeholder="Escoja una opción..." class="chzn-select span8" tabindex="2" name="cliente">
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM clientes",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
+											$conOp = mysqli_query($conexionBdPrincipal,"SELECT * FROM clientes");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
 												if($datosUsuarioActual[3]!=1){
-													$numZ = mysql_num_rows(mysql_query("SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$_SESSION["id"]."' AND zpu_zona='".$resOp['cli_zona']."'",$conexion));
+													$consultaNumZonas=mysqli_query($conexionBdPrincipal,"SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$_SESSION["id"]."' AND zpu_zona='".$resOp['cli_zona']."'");
+													$numZ = mysqli_num_rows($consultaNumZonas);
 													if($numZ==0) continue;
 												}
 											?>
@@ -125,12 +104,13 @@ include("includes/js-formularios.php");
 										<select data-placeholder="Escoja una opción..." class="chzn-select span10" tabindex="2" name="producto[]" multiple>
 											<option value=""></option>
                                             <?php
-											$conOp = mysql_query("SELECT * FROM productos_soptec 
+											$conOp = mysqli_query($conexionBdPrincipal,"SELECT * FROM productos_soptec 
 											LEFT JOIN productos_categorias ON catp_id=prod_categoria
-											ORDER BY prod_nombre",$conexion);
-											while($resOp = mysql_fetch_array($conOp)){
-												$materiales = mysql_num_rows(mysql_query("SELECT * FROM productos_materiales 
-												WHERE ppmt_producto='".$resOp['prod_id']."'",$conexion));
+											ORDER BY prod_nombre");
+											while($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)){
+												$consultaMaterial=mysqli_query($conexionBdPrincipal,"SELECT * FROM productos_materiales 
+												WHERE ppmt_producto='".$resOp['prod_id']."'");
+												$materiales = mysqli_num_rows($consultaMaterial);
 												if($materiales == 0) {continue;}
 											?>
                                             	<option value="<?=$resOp['prod_id'];?>"><?=$resOp['prod_referencia']." - ".$resOp['prod_nombre'].", ".$resOp['catp_nombre']." (".$materiales." Mat.)";?></option>
--- usuarios/facturacion.php
+++ usuarios/facturacion.php
@@ -1,13 +1,10 @@
-<?php include("sesion.php");?>
-<?php
+<?php 
+include("sesion.php");
+
 $idPagina = 20;
 $paginaActual['pag_nombre'] = "Productos a clientes";
-?>
-<?php include("includes/verificar-paginas.php");?>
-<?php include("includes/head.php");?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPagina."', now(),'".$_SERVER['HTTP_REFERER']."')",$conexion);
-if(mysql_errno()!=0){echo mysql_error(); exit();}
+include("includes/verificar-paginas.php");
+include("includes/head.php");
 ?>
 <!-- styles -->
 
@@ -28,46 +25,9 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 <script src="js/respond.min.js"></script>
 <script src="js/ios-orientationchange-fix.js"></script>
 <script type="text/javascript">
-            /*$( function () {
-		  // Set the classes that TableTools uses to something suitable for Bootstrap
-		  $.extend( true, $.fn.DataTable.TableTools.classes, {
-			  "container": "btn-group",
-			  "buttons": {
-				  "normal": "btn",
-				  "disabled": "btn disabled"
-			  },
-			  "collection": {
-				  "container": "DTTT_dropdown dropdown-menu",
-				  "buttons": {
-					  "normal": "",
-					  "disabled": "disabled"
-				  }
-			  }
-		  } );
-		  // Have the collection use a bootstrap compatible dropdown
-		  $.extend( true, $.fn.DataTable.TableTools.DEFAULTS.oTags, {
-			  "collection": {
-				  "container": "ul",
-				  "button": "li",
-				  "liner": "a"
-			  }
-		  } );
-		  });
-		  */
             $(function () {
                 $('#data-table').dataTable({
                     "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>"
-                    /*"oTableTools": {
-			"aButtons": [
-				"copy",
-				"print",
-				{
-					"sExtends":    "collection",
-					"sButtonText": 'Save <span class="caret" />',
-					"aButtons":    [ "csv", "xls", "pdf" ]
-				}
-			]
-		}*/
                 });
             });
             $(function () {
@@ -109,16 +69,6 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
             <p>
             <a href="javascript:history.go(-1);" class="btn btn-primary"><i class="icon-arrow-left"></i> Regresar</a>
             <a href="facturacion-agregar.php?cte=<?=$_GET["cte"];?>" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a>
-				<!--
-            <div class="btn-group">
-							<button class="btn btn-primary">Acciones</button>
-							<button data-toggle="dropdown" class="btn btn-primary dropdown-toggle"><span class="caret"></span>
-							</button>
-							<ul class="dropdown-menu">
-                            	<li><a href="facturacion-filtros.php">Imprimir informes</a></li>
-							</ul>
-						</div>
--->
             </p>
 			<div class="row-fluid">
 				<div class="span12">
@@ -140,25 +90,26 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 							<tbody>
                             <?php
 							if(isset($_GET["cte"]) and $_GET["cte"]!=""){
-								$consulta = mysql_query("SELECT * FROM facturacion
+								$consulta = mysqli_query($conexionBdPrincipal,"SELECT * FROM facturacion
 								INNER JOIN clientes ON cli_id=fact_cliente
-								INNER JOIN localidad_ciudades ON ciu_id=cli_ciudad
-								INNER JOIN localidad_departamentos ON dep_id=ciu_departamento
-								WHERE fact_cliente='".$_GET["cte"]."'",$conexion);
+								INNER JOIN ".BDADMIN.".localidad_ciudades ON ciu_id=cli_ciudad
+								INNER JOIN ".BDADMIN.".localidad_departamentos ON dep_id=ciu_departamento
+								WHERE fact_cliente='".$_GET["cte"]."'");
 							}else{
-								$consulta = mysql_query("SELECT * FROM facturacion
+								$consulta = mysqli_query($conexionBdPrincipal,"SELECT * FROM facturacion
 								INNER JOIN clientes ON cli_id=fact_cliente
-								INNER JOIN localidad_ciudades ON ciu_id=cli_ciudad
-								INNER JOIN localidad_departamentos ON dep_id=ciu_departamento
-								",$conexion);
+								INNER JOIN ".BDADMIN.".localidad_ciudades ON ciu_id=cli_ciudad
+								INNER JOIN ".BDADMIN.".localidad_departamentos ON dep_id=ciu_departamento
+								");
 							}
 							$no = 1;
-							while($res = mysql_fetch_array($consulta)){
-								$Cpd = mysql_query("SELECT * FROM facturacion_productos
+							while($res = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
+								$Cpd = mysqli_query($conexionBdPrincipal,"SELECT * FROM facturacion_productos
 								INNER JOIN productos_soptec ON prod_id=fpp_producto
-								WHERE fpp_factura='".$res['fact_id']."'",$conexion);
+								WHERE fpp_factura='".$res['fact_id']."'");
 								
-								$abonos = mysql_fetch_array(mysql_query("SELECT sum(fpab_valor) FROM facturacion_abonos WHERE fpab_factura='".$res['fact_id']."'",$conexion));
+								$consultaAbonos=mysqli_query($conexionBdPrincipal,"SELECT sum(fpab_valor) FROM facturacion_abonos WHERE fpab_factura='".$res['fact_id']."'");
+								$abonos = mysqli_fetch_array($consultaAbonos, MYSQLI_BOTH);
 								
 								
 								$impuestos = $res['fact_valor'] * $res['fact_impuestos']/100;
@@ -170,7 +121,8 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 								$saldoFinal = $valorReal - $abonos[0];
 
 								if($datosUsuarioActual[3]!=1){
-									$numZ = mysql_num_rows(mysql_query("SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$_SESSION["id"]."' AND zpu_zona='".$res['cli_zona']."'",$conexion));
+									$consultaNumZonas=mysqli_query($conexionBdPrincipal,"SELECT * FROM zonas_usuarios WHERE zpu_usuario='".$_SESSION["id"]."' AND zpu_zona='".$res['cli_zona']."'");
+									$numZ = mysqli_num_rows($consultaNumZonas);
 									if($numZ==0) continue;
 								}
 							?>
@@ -183,14 +135,14 @@ if(mysql_errno()!=0){echo mysql_error(); exit();}
 									<?php echo "<b>Nombre</b>: ". $res['cli_nombre']."<br>";?>
                                     <?php echo "<b>Ciudad</b>: ". $res['ciu_nombre'].", ".$res['dep_nombre']." (<b>03".$res['dep_indicativo']."</b>)";?>
                                     <h4 style="margin-top:10px;">
-                                	<a href="facturacion-editar.php?id=<?=$res['fact_id'];?>" data-toggle="tooltip" title="Editar"><i class="icon-edit"></i></a>&nbsp;
-                                    <a href="sql.php?id=<?=$res['fact_id'];?>&get=6" onClick="if(!confirm('Desea eliminar el registro?')){return false;}" data-toggle="tooltip" title="Eliminar"><i class="icon-remove-sign"></i></a>
+                                	<a href="facturacion-editar.php?id=<?=$res['fact_id'];?>&cte=<?=$_GET['cte'];?>" data-toggle="tooltip" title="Editar"><i class="icon-edit"></i></a>&nbsp;
+                                    <a href="bd_delete/facturacion-eliminar.php?id=<?=$res['fact_id'];?>&cte=<?=$_GET['cte'];?>" onClick="if(!confirm('Desea eliminar el registro?')){return false;}" data-toggle="tooltip" title="Eliminar"><i class="icon-remove-sign"></i></a>
                                 </h4>
                                 </td>
 
                                 
                                 <td><?php 
-								while($pds = mysql_fetch_array($Cpd)){
+								while($pds = mysqli_fetch_array($Cpd, MYSQLI_BOTH)){
 									echo '<a href="productos-materiales.php?pdto='.$pds['prod_id'].'" title="Ver materiales">'.$pds['prod_nombre']."</a><br>";
 								}
 								?></td>
