1a0d1c6f58353e05b349726fc7f18566a3c8b0d8
Enuar Lara <enuar2110@gmail.com>
2023-04-26 11:51:23 -0500

    Se actualizan consultas para Proveedores

--- usuarios/proveedores-editar.php
+++ usuarios/proveedores-editar.php
@@ -1,37 +1,20 @@
-<?php include("sesion.php"); ?>
-<?php
-$resultadoD = mysql_fetch_array(mysql_query("SELECT * FROM proveedores WHERE prov_id='" . $_GET["id"] . "'", $conexion));
+<?php 
+include("sesion.php");
+
 
 $idPagina = 125;
-$paginaActual['pag_nombre'] = "Editar proveedores";
-?>
-<?php include("includes/verificar-paginas.php"); ?>
-<?php include("includes/head.php"); ?>
-<?php
-mysql_query("INSERT INTO historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_pagina_anterior)VALUES('" . $_SESSION["id"] . "', '" . $_SERVER['PHP_SELF'] . "?" . $_SERVER['QUERY_STRING'] . "', '" . $idPagina . "', now(),'" . $_SERVER['HTTP_REFERER'] . "')", $conexion);
-if (mysql_errno() != 0) {
-	echo mysql_error();
-	exit();
-}
+
+include("includes/verificar-paginas.php");
+include("includes/head.php");
+
+$consulta=mysqli_query($conexionBdPrincipal,"SELECT * FROM proveedores WHERE prov_id='" . $_GET["id"] . "'");
+$resultadoD = mysqli_fetch_array($consulta, MYSQLI_BOTH);
 ?>
 
 <!-- styles -->
 
-<!--[if IE 7]>
-<link rel="stylesheet" href="css/font-awesome-ie7.min.css">
-<![endif]-->
 <link href="css/chosen.css" rel="stylesheet">
 
-
-<!--[if IE 7]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie7.css" />
-<![endif]-->
-<!--[if IE 8]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie8.css" />
-<![endif]-->
-<!--[if IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie/ie9.css" />
-<![endif]-->
 
 <!--============ javascript ===========-->
 <script src="js/jquery.js"></script>
@@ -156,7 +139,7 @@ if (mysql_errno() != 0) {
 			"&opcion=" + (opcion);
 		$.ajax({
 			type: "POST",
-			url: "ajax-clientes-verificar.php",
+			url: "ajax/ajax-clientes-verificar.php",
 			data: datos,
 			success: function(data) {
 				$('#resp').empty().hide().html(data).show(1);
@@ -182,14 +165,6 @@ if (mysql_errno() != 0) {
 					<div class="span12">
 						<div class="primary-head">
 							<h3 class="page-header"><?= $paginaActual['pag_nombre']; ?></h3>
-
-							<ul class="top-right-toolbar">
-								<li><a data-toggle="dropdown" class="dropdown-toggle blue-violate" href="#" title="Users"><i class="icon-user"></i></a>
-								</li>
-								<li><a href="#" class="green" title="Upload"><i class=" icon-upload-alt"></i></a></li>
-								<li><a href="#" class="bondi-blue" title="Settings"><i class="icon-cogs"></i></a></li>
-							</ul>
-
 						</div>
 						<ul class="breadcrumb">
 							<li><a href="index.php" class="icon-home"></a><span class="divider "><i class="icon-angle-right"></i></span></li>
@@ -205,8 +180,7 @@ if (mysql_errno() != 0) {
 								<h3> <?= $paginaActual['pag_nombre']; ?></h3>
 							</div>
 							<div class="widget-container">
-								<form class="form-horizontal" method="post" action="sql.php" enctype="multipart/form-data">
-									<input type="hidden" name="idSql" value="71">
+								<form class="form-horizontal" method="post" action="bd_update/proveedores-actualizar.php" enctype="multipart/form-data">
 									<input type="hidden" name="id" value="<?= $_GET["id"]; ?>">
 
 									<fieldset class="default">
@@ -271,12 +245,10 @@ if (mysql_errno() != 0) {
 												<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="pais">
 													<option value="1"></option>
 													<?php
-													$conOp = mysql_query("SELECT * FROM localidad_paises ORDER BY pais_nombre", $conexion);
-													while ($resOp = mysql_fetch_array($conOp)) {
+													$conOp = mysqli_query($conexionBdAdmin,"SELECT * FROM localidad_paises ORDER BY pais_nombre");
+													while ($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)) {
 													?>
-														<option value="<?= $resOp[0]; ?>" <?php if ($resultadoD['prov_pais'] == $resOp['pais_id']) {
-																							echo "selected";
-																						} ?>><?= $resOp['pais_nombre']; ?></option>
+														<option value="<?= $resOp[0]; ?>" <?php if ($resultadoD['prov_pais'] == $resOp['pais_id']) {echo "selected";} ?>><?= $resOp['pais_nombre']; ?></option>
 													<?php
 													}
 													?>
@@ -291,12 +263,10 @@ if (mysql_errno() != 0) {
 												<select data-placeholder="Escoja una opción..." class="chzn-select span4" tabindex="2" name="ciudad">
 													<option value="1"></option>
 													<?php
-													$conOp = mysql_query("SELECT * FROM localidad_ciudades INNER JOIN localidad_departamentos ON dep_id=ciu_departamento ORDER BY ciu_nombre", $conexion);
-													while ($resOp = mysql_fetch_array($conOp)) {
+													$conOp = mysqli_query($conexionBdAdmin,"SELECT * FROM localidad_ciudades INNER JOIN localidad_departamentos ON dep_id=ciu_departamento ORDER BY ciu_nombre");
+													while ($resOp = mysqli_fetch_array($conOp, MYSQLI_BOTH)) {
 													?>
-														<option value="<?= $resOp['ciu_id']; ?>" <?php if ($resultadoD['prov_ciudad'] == $resOp['ciu_id']) {
-																										echo "selected";
-																									} ?>><?= $resOp['ciu_nombre'] . ", " . $resOp['dep_nombre']; ?></option>
+														<option value="<?= $resOp['ciu_id']; ?>" <?php if ($resultadoD['prov_ciudad'] == $resOp['ciu_id']) {echo "selected";} ?>><?= $resOp['ciu_nombre'] . ", " . $resOp['dep_nombre']; ?></option>
 													<?php
 													}
 													?>
@@ -332,59 +302,6 @@ if (mysql_errno() != 0) {
 						</div>
 					</div>
 				</div>
-
-
-				<!--
-             <div class="row-fluid">
-				<div class="span12">
-					<div class="content-widgets gray">
-						<div class="widget-head bondi-blue">
-							<h3> <?= $paginaActual['pag_nombre']; ?></h3>
-						</div>
-						<div class="widget-container">
-                                
-                                <div class="control-group">
-									<label class="control-label">Alcance</label>
-									<div class="controls">
-										<textarea rows="5" cols="80" style="width: 100%" class="tinymce-simple" name="alcance"></textarea>
-									</div>
-								</div>
-                                
-                                <div class="control-group">
-									<label class="control-label">Primera auditoría</label>
-									<div class="controls">
-										<textarea rows="5" cols="80" style="width: 100%" class="tinymce-simple" name="pa"></textarea>
-									</div>
-								</div>
-                                
-                                <div class="control-group">
-									<label class="control-label">Segunda auditoría</label>
-									<div class="controls">
-										<textarea rows="5" cols="80" style="width: 100%" class="tinymce-simple" name="sa"></textarea>
-									</div>
-								</div>
-                                
-                                <div class="control-group">
-									<label class="control-label">Renovación</label>
-									<div class="controls">
-										<textarea rows="5" cols="80" style="width: 100%" class="tinymce-simple" name="renovacion"></textarea>
-									</div>
-								</div>
-
-								<div class="form-actions">
-									<a href="clientes.php" class="btn btn-primary"><i class="icon-arrow-left"></i> Regresar</a>
-                                    <button type="submit" class="btn btn-info"><i class="icon-save"></i> Guardar cambios</button>
-								</div>
-                              
-                            </form>    
-
-						</div>
-					</div>
-				</div>
-			</div>
-            -->
-
-
 			</div>
 		</div>
 		<?php include("includes/pie.php"); ?>
