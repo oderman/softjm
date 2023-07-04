b8e18f33d085b42b92f018eafa8dcfb427bb0708
Enuar Lara <enuar2110@gmail.com>
2023-07-04 11:47:40 -0500

    Se añaden constantes para nuevos dominios

--- constantes.php
+++ constantes.php
@@ -11,4 +11,16 @@ switch($_SERVER['HTTP_HOST']){
         define('REDIRECT_ROUTE', 'https://nuevo.orioncrm.com.co/softjmgit');
         error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
+
+	case 'developer.orioncrm.com.co':
+        define('RUTA_PROYECTO', '/home4/orioncrmcom/public_html/developer.orioncrm.com.co/softjm');
+        define('REDIRECT_ROUTE', 'https://developer.orioncrm.com.co/softjm');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+        break;
+
+	case 'jm.orioncrm.com.co':
+        define('RUTA_PROYECTO', '/home4/orioncrmcom/public_html/jm.orioncrm.com.co/softjm');
+        define('REDIRECT_ROUTE', 'https://jm.orioncrm.com.co/softjm');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+        break;
 }
