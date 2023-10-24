5f1e43d1099ba5a666be6c4fc5df2185d30d1901
Jhon Oderman <jhooderman@gmail.com>
2023-10-24 10:40:27 -0400

    DEV -  Obtener los datos del usuario de la sesion previamente creada

--- usuarios/includes/sesion-usuario-actual.php
+++ usuarios/includes/sesion-usuario-actual.php
@@ -1,27 +1,23 @@
 
 <?php
-    //USUARIO ACTUAL
-	$consultaUsuarioActual = $conexionBdPrincipal->query("SELECT * FROM usuarios WHERE usr_id='".$_SESSION["id"]."'");
-	$numUsuarioActual = $consultaUsuarioActual->num_rows;
+$datosUsuarioActual = $_SESSION["dataAdicional"]["datos_usuario_actual"];
 
-	if($numUsuarioActual == 0){
-		echo "<span style='font-family:Arial; color:red;'>El usuario con ID <b>".$_SESSION["id"]."</b> no existe.</samp>";
-		exit();
-	}
+if( empty($datosUsuarioActual) ){
+	echo "<span style='font-family:Arial; color:red;'>El usuario con ID <b>".$_SESSION["id"]."</b> no existe.</samp>";
+	exit();
+}
 
-	$datosUsuarioActual = mysqli_fetch_array($consultaUsuarioActual, MYSQLI_BOTH);
-
-	//SABER SI ESTA BLOQUEADO
-	if($datosUsuarioActual['usr_bloqueado']==1)
-	{
+//SABER SI ESTA BLOQUEADO
+if($datosUsuarioActual['usr_bloqueado'] == 1)
+{
 ?>		
-		<span style='font-family:Arial; color:red;'>Su usuario ha sido bloqueado. Por tanto no tiene permisos para acceder al Sistema.</samp>
-        <script type="text/javascript">
-		function sacar(){
-			window.location.href="../salir.php";
-		} 
-		setInterval('sacar()', 3000);
-        </script>
+	<span style='font-family:Arial; color:red;'>Su usuario ha sido bloqueado. Por tanto no tiene permisos para acceder al Sistema.</samp>
+	<script type="text/javascript">
+	function sacar(){
+		window.location.href="../salir.php";
+	} 
+	setInterval('sacar()', 3000);
+	</script>
 <?php	
-    	exit();		
-	}
+	exit();		
+}
