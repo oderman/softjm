23984f9b78511269987e6ff7b40a38222b0581a3
Jhon Oderman <jhooderman@gmail.com>
2023-10-24 06:46:20 -0400

    DEV -  Remover datos sensibles

--- conexion.php
+++ conexion.php
@@ -1,12 +1,6 @@
 <?php
 require_once("constantes.php");
 
-const SERVER = 'orioncrm.com.co' ;
-const USER = 'orioncrmcom_dev';
-const PASS = 'Y7)octcBq]#5';
-const MAINBD = 'orioncrmcom_dev_jm_crm';
-const BDADMIN = 'orioncrmcom_dev_crm_admin';
-
 $conexionBdPrincipal = new mysqli(SERVER, USER, PASS, MAINBD);
-$conexionBdAdmin = new mysqli(SERVER, USER, PASS, BDADMIN);
-$conexionBdStore = new mysqli(SERVER, USER, PASS, 'orioncrmcom_dev_crm_store');
+$conexionBdAdmin     = new mysqli(SERVER, USER, PASS, BDADMIN);
+$conexionBdStore     = new mysqli(SERVER, USER, PASS, BDSTORE);
--- constantes.php
+++ constantes.php
@@ -1,26 +1,2 @@
 <?php
-switch($_SERVER['HTTP_HOST']){
-	case 'localhost':
-        define('RUTA_PROYECTO', 'C:/xampp/htdocs/softjm');
-        define('REDIRECT_ROUTE', 'http://localhost/softjm');
-        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
-        break;
-
-	case 'nuevo.orioncrm.com.co':
-        define('RUTA_PROYECTO', '/home4/orioncrmcom/public_html/nuevo.orioncrm.com.co/softjmgit');
-        define('REDIRECT_ROUTE', 'https://nuevo.orioncrm.com.co/softjmgit');
-        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
-        break;
-
-	case 'developer.orioncrm.com.co':
-        define('RUTA_PROYECTO', '/home4/orioncrmcom/developer.orioncrm.com.co/softjm');
-        define('REDIRECT_ROUTE', 'https://developer.orioncrm.com.co/softjm');
-        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
-        break;
-
-	case 'jm.orioncrm.com.co':
-        define('RUTA_PROYECTO', '/home4/orioncrmcom/jm.orioncrm.com.co/softjm');
-        define('REDIRECT_ROUTE', 'https://jm.orioncrm.com.co/softjm');
-        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
-        break;
-}
+require_once ('sensitive.php');
