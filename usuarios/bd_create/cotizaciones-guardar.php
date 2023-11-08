adc4cd2316a5e76f01a0a790cef3110d31d0a354
sllower166 <git>
2023-11-08 10:34:50 -0500

    OR-19 Agg idEmpresa queries cotizaciones  guardar,replicar,formato

--- usuarios/bd_create/cotizaciones-guardar.php
+++ usuarios/bd_create/cotizaciones-guardar.php
@@ -2,7 +2,7 @@
 require_once("../sesion.php");
 
 $idPagina = 29;
-
+$idEmpresa = $_SESSION["dataAdicional"]["id_empresa"];
 if(isset($_POST["envio"])){
     $envio=$_POST["envio"];
 }else{
@@ -12,7 +12,7 @@ if(isset($_POST["envio"])){
 $consulta=$conexionBdPrincipal->query("SELECT * FROM clientes WHERE cli_id='".$_POST["cliente"]."'");
 $datosCliente = mysqli_fetch_array($consulta, MYSQLI_BOTH);
 
-    $conexionBdPrincipal->query("INSERT INTO cotizacion(cotiz_fecha_propuesta, cotiz_cliente, cotiz_fecha_vencimiento, cotiz_vendedor, cotiz_creador, cotiz_sucursal, cotiz_contacto, cotiz_forma_pago, cotiz_fecha_creacion, cotiz_moneda, cotiz_observaciones, cotiz_envio, cotiz_proveedor)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["influyente"] . "','" . $_SESSION["id"] . "','" . $_POST["sucursal"] . "','" . $_POST["contacto"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "','" . $envio . "','" . $_POST["proveedor"] . "')");
+    $conexionBdPrincipal->query("INSERT INTO cotizacion(cotiz_fecha_propuesta, cotiz_cliente, cotiz_fecha_vencimiento, cotiz_vendedor, cotiz_creador, cotiz_sucursal, cotiz_contacto, cotiz_forma_pago, cotiz_fecha_creacion, cotiz_moneda, cotiz_observaciones, cotiz_envio, cotiz_proveedor, cotiz_id_empresa)VALUES('" . $_POST["fechaPropuesta"] . "','" . $_POST["cliente"] . "','" . $_POST["fechaVencimiento"] . "','" . $_POST["influyente"] . "','" . $_SESSION["id"] . "','" . $_POST["sucursal"] . "','" . $_POST["contacto"] . "','" . $_POST["formaPago"] . "',now(),'" . $_POST["moneda"] . "','" . $_POST["notas"] . "','" . $envio . "','" . $_POST["proveedor"] . "','" . $idEmpresa . "' )");
     $idInsert = mysqli_insert_id($conexionBdPrincipal);
 
     require("guardar-productos-cotizacion.php");
