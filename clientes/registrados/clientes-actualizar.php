76224895313f0a2e834fe28226c228802bf02ba5
oscarleo898998 <oscarleo898998@gmail.com>
2023-11-23 08:29:04 -0500

    OR-58 correccion de link session and verificar pag

--- clientes/registrados/clientes-actualizar.php
+++ clientes/registrados/clientes-actualizar.php
@@ -1,6 +1,6 @@
-<?php include("usuarios/sesion.php");
+<?php include("sesion.php");
 $idPagina = 329;
-include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
+include("verificar-paginas.php");
 ?>
 <?php
 mysqli_query($conexionBdPrincipal,"UPDATE clientes SET cli_usuario='".$_POST["usuario"]."', cli_clave='".$_POST["clave"]."', cli_nombre='".$_POST["nombre"]."', cli_email='".$_POST["email"]."' WHERE cli_id='".$_SESSION["id_cliente"]."'");
--- clientes/registrados/contactos-actualizar.php
+++ clientes/registrados/contactos-actualizar.php
@@ -1,6 +1,6 @@
-<?php include("usuarios/sesion.php");
+<?php include("sesion.php");
 $idPagina = 334;
-include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
+include("verificar-paginas.php");
 ?>
 <?php
 mysqli_query($conexionBdPrincipal,"UPDATE contactos SET cont_nombre='".$_POST["nombre"]."', cont_email='".$_POST["email"]."', cont_rol='".$_POST["rol"]."', cont_celular='".$_POST["celular"]."', cont_sucursal='".$_POST["sucursal"]."' WHERE cont_id='".$_POST["id"]."'");
