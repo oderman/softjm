189bbf53e7a95a8fb2f647b05e806ddf3364cff9
oscarleo898998 <oscarleo898998@gmail.com>
2023-11-23 08:31:08 -0500

    OR 58 se corrige vinculo sesion y error email nulo

--- clientes/registrados/solicitar-copia-certificado.php
+++ clientes/registrados/solicitar-copia-certificado.php
@@ -1,19 +1,28 @@
-<?php include("usuarios/sesion.php");
+<?php include("sesion.php");
 $idPagina = 332;
-$remision = mysqli_fetch_array(mysqli_query($conexionBdPrincipal,"SELECT * FROM remisiones 
-	INNER JOIN clientes ON cli_id=rem_cliente
-	WHERE rem_id='".$_POST["idRem"]."'"), MYSQLI_BOTH);
-	
-	$contactoV = mysqli_fetch_array(mysqli_query($conexionBdPrincipal,"SELECT * FROM contactos 
-	WHERE cont_email='".trim($_POST["email"])."' AND cont_cliente_principal='".$_SESSION["id_cliente"]."'", MYSQLI_BOTH));
-	if($contactoV[0]==""){
-		mysqli_query($conexionBdPrincipal,"INSERT INTO contactos(cont_nombre, cont_email, cont_cliente_principal)VALUES('".$_POST["nombre"]."','".$_POST["email"]."','".$_SESSION["id_cliente"]."')");
-		$idContacto = mysqli_insert_id($conexionBdPrincipal);
-		$contacto = $idContacto;
-	}else{
-		$contacto = $contactoV[0];	
-	}
-	
+$remision = mysqli_fetch_array(mysqli_query($conexionBdPrincipal, "SELECT * FROM remisiones 
+    INNER JOIN clientes ON cli_id=rem_cliente
+    WHERE rem_id='" . $_POST["idRem"] . "'"), MYSQLI_BOTH);
+
+// Check if email is set before using trim
+$email = isset($_POST["email"]) ? trim($_POST["email"]) : '';
+
+$contactoV = mysqli_fetch_array(mysqli_query($conexionBdPrincipal, "SELECT * FROM contactos 
+    WHERE cont_email='" . $email . "' AND cont_cliente_principal='" . $_SESSION["id_cliente"] . "'", MYSQLI_USE_RESULT));
+
+// Check if the query result is not empty before accessing $contactoV[0]
+if (empty($contactoV)) {
+    mysqli_query($conexionBdPrincipal, "INSERT INTO contactos(cont_nombre, cont_email, cont_cliente_principal) 
+        VALUES('" . $_POST["nombre"] . "','" . $_POST["email"] . "','" . $_SESSION["id_cliente"] . "')");
+    $idContacto = mysqli_insert_id($conexionBdPrincipal);
+    $contacto = $idContacto;
+} else {
+    $contacto = $contactoV[0];
+}
+
+
+
+
 	
 	mysqli_query($conexionBdPrincipal,"INSERT INTO clientes_tikets(tik_asunto_principal, tik_tipo_tiket, tik_fecha_creacion, tik_usuario_responsable, tik_estado, tik_cliente, tik_prioridad, tik_canal)
 	VALUES('SOLICITUD COPIA CERTIFICADO',2,now(),27,1,'".$_SESSION["id_cliente"]."',1,7)");
