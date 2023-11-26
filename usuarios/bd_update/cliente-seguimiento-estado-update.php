ce551bcebe592e90b537190079c163cd4e734a4a
oscarleo898998 <oscarleo898998@gmail.com>
2023-11-26 15:34:09 -0500

    OR-26-se incluye pagina historial de acciones

--- usuarios/bd_update/cliente-seguimiento-estado-update.php
+++ usuarios/bd_update/cliente-seguimiento-estado-update.php
@@ -6,7 +6,7 @@ include(RUTA_PROYECTO."/usuarios/includes/verificar-paginas.php");
 
 mysqli_query($conexionBdPrincipal,"UPDATE cliente_seguimiento SET cseg_realizado=1 WHERE cseg_id='" . $_GET["id"] . "'");
 	
-	
+include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");	
 
 echo '<script type="text/javascript">window.location.href="' . $_SERVER['HTTP_REFERER'] . '";</script>';
 	exit();
