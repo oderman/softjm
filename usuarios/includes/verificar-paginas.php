3d3dc840c61adc4852cdc9beb8112053dac1d9d3
sllower166 <git>
2023-11-28 10:05:50 -0500

    OR-72 Solucion Error al verificar paginas con multiples roles

--- usuarios/includes/verificar-paginas.php
+++ usuarios/includes/verificar-paginas.php
@@ -59,24 +59,3 @@ if (!$paso) {
 	exit();	
 }
 
-//PAGINAS A LAS QUE TIENE PERMISO EL ROL DEL USUARIO
-$consultaPaginaUsuario = $conexionBdPrincipal->query("SELECT * FROM paginas_perfiles
-WHERE pper_tipo_usuario='".$datosUsuarioActual[3]."' AND pper_pagina='".$idPagina."'");
-$numPaginaUsuario = $consultaPaginaUsuario->num_rows;
-
-/*
-La segunda parte de la condición es para darle permiso a los administradores 
-a todas las paginas del sistema.
-*/
-if($numPaginaUsuario == 0 and $datosUsuarioActual[3]!=1){
-?>
-		<span style='font-family:Arial; color:red;'>No tienes permiso para acceder a este pagina. Ser&aacute;s redireccionado al inicio...</samp>
-        <script type="text/javascript">
-		function sacar(){
-			window.location.href="index.php";
-		} 
-		setInterval('sacar()', 3000);
-        </script>
-<?php
-	exit();	
-}
