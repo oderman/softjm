4be10fb2cc5807fba6121d3a70a18d2362e271b3
sllower166 <git>
2023-11-29 12:27:44 -0500

    OR-72 Comprobar acciones por roles en Zonas

--- usuarios/zonas-editar.php
+++ usuarios/zonas-editar.php
@@ -63,7 +63,13 @@ include("includes/js-formularios.php");
 					</ul>
 				</div>
 			</div>
-            <p><a href="zonas-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a></p>
+        <p>
+					<?php
+						if (Modulos::validarRol([48], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion)) {
+							echo '<a href="zonas-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a>';
+						}
+					?>	
+				</p>
             <?php include("includes/notificaciones.php");?>
 			<div class="row-fluid">
 				<div class="span12">
