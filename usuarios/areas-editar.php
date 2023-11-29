0eef64eacd764919c9491ee6bbf8eb5f9a369de5
sllower166 <git>
2023-11-29 12:26:47 -0500

    OR-72 Comprobar acciones por roles en Areas

--- usuarios/areas-editar.php
+++ usuarios/areas-editar.php
@@ -75,7 +75,11 @@ include("includes/js-formularios.php");
 					</ul>
 				</div>
 			</div>
-            <p><a href="areas-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a></p>
+				<?php
+					if (Modulos::validarRol([162], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion)) {
+						echo '<p><a href="areas-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar nuevo</a></p>';
+					}
+				?>	
             <?php include("includes/notificaciones.php");?>
 			<div class="row-fluid">
 				<div class="span12">
