1a19bf8ba627347cbe48c526dd5b08359641143f
sllower166 <git>
2023-12-13 09:43:05 -0500

    OR-30 Pagina para redirecionar a tutoriales completos

--- constantes.php
+++ constantes.php
@@ -4,3 +4,4 @@ define('ID_DOC_COTIZACION', 1);
 define('ID_DOC_REMISION', 2);
 define('ID_DOC_PEDIDO', 3);
 define('ID_DOC_FACTURA', 4);
+define('URL_TUTORIAL', 'https://www.loom.com/share/308bdd148ddc4bffb2af76e27e3d5139');
