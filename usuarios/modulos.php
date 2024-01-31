3d950cbba66da60d417c79728323223437720c76
Jhon Oderman Mejia <jhooderman@gmail.com>
2024-01-31 07:06:47 -0500

    Validar la propiedad de la empresa en los modulos

--- usuarios/modulos.php
+++ usuarios/modulos.php
@@ -57,10 +57,10 @@ include("includes/head.php");
 		<div class="main-wrapper">
 			<div class="container-fluid">
 				<div class="row-fluid ">
-				<?php if(Modulos::validarRol([182], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion)){
+				<?php if(Modulos::validarRol([182], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion) && $idEmpresa == ID_COMPANY_OWNER) {
                 ?>
 					<p><a href="modulos-agregar.php" class="btn btn-danger"><i class="icon-plus"></i> Agregar Modulo</a></p>
-					<?php } ?>
+				<?php } ?>
 					<div class="row-fluid">
 						<div class="span12">
 							<div class="content-widgets light-gray">
@@ -75,12 +75,20 @@ include("includes/head.php");
 												<th>Nº</th>
 												<th>Nombre</th>
 												<th>Modulo Padre</th>
-												<th></th>
+												<?php if ($idEmpresa == ID_COMPANY_OWNER) {?>
+													<th></th>
+												<?php }?>
 											</tr>
 										</thead>
 										<tbody>
 											<?php
-											$consulta = $conexionBdAdmin->query("SELECT * FROM modulos");
+											if($idEmpresa == ID_COMPANY_OWNER) {
+												$consulta = $conexionBdAdmin->query("SELECT * FROM modulos");
+											} else {
+												$consulta = $conexionBdAdmin->query("SELECT * FROM modulos
+												INNER JOIN modulos_empresa ON mxe_id_empresa = '".$idEmpresa."' AND mxe_id_modulo = mod_id
+												");
+											}
 											$no = 1;
 											while($res = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
 												$nombreModuloPadre="";
@@ -94,13 +102,14 @@ include("includes/head.php");
 												<td><?=$no;?></td>
 												<td><?=$res['mod_nombre'];?></td>
 												<td><?=$nombreModuloPadre?></td>
+												<?php if(Modulos::validarRol([184], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion) && $idEmpresa == ID_COMPANY_OWNER) {
+												?>
 												<td><h4>
-												<?php if(Modulos::validarRol([184], $conexionBdPrincipal, $conexionBdAdmin, $datosUsuarioActual, $configuracion)){
-               									 ?>
 													<a href="modulos-editar.php?id=<?=$res[0];?>" data-toggle="tooltip" title="Editar"><i class="icon-edit"></i></a>
-												<?php } ?>
+												
 													<!-- <a href="bd_delete/modulos-eliminar.php?id=<?=$res[0];?>" onClick="if(!confirm('Desea eliminar el registro?')){return false;}" data-toggle="tooltip" title="Eliminar"><i class="icon-remove-sign"></i></a> -->
 												</h4></td>
+												<?php } ?>
 											</tr>
 											<?php $no++;}?>
 										</tbody>
