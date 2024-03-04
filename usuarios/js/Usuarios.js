20fc9f5599d4d6cce3ee554c56287994e7f5d7f9
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-03-03 23:03:00 -0500

    OR-88 - Se crea función js para validar usuario

---
+++ usuarios/js/Usuarios.js
@@ -0,0 +1,36 @@
+/**
+ * Esta función hace una petición asincrona y recibe una respuesta.
+ * @param {array} datos 
+ */
+function validarUsuario(datos) {
+    var usuario = datos.value;
+    var idUsuario = datos.getAttribute("data-id-usuario");
+    
+    if(usuario!=""){
+
+        fetch('ajax/ajax-comprobar-usuario.php?usuario=' + usuario + '&idUsuario=' + idUsuario, {
+            method: 'GET'
+        })
+        .then(response => response.json()) // Convertir la respuesta a objeto JSON
+        .then(data => {
+                if (data.success == 1) {
+                    $("#respuestaUsuario").html(data.message);
+                    $("input").attr('disabled', true); 
+                    $("input#usuario").attr('disabled',false); 
+                    $("#btnEnviar").attr('disabled', true); 
+                } else {
+                    $("#respuestaUsuario").html(data.message);
+                    $("input").attr('disabled', false); 
+                    $("#btnEnviar").attr('disabled', false); 
+                }
+        })
+        .catch(error => {
+            // Manejar errores
+            console.error('Error:', error);
+        });
+    } else {
+        $("#respuestaUsuario").html("");
+        $("input").attr('disabled', false); 
+        $("#btnEnviar").attr('disabled', false); 
+    }
+}
