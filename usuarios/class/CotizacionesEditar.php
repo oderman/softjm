fa8e553318e8b04c5f42dc35cfc77089f29ee7ef
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-03-05 16:57:54 -0500

    OR-90 - Se crea metodo para crear tr para los servicios

--- usuarios/class/CotizacionesEditar.php
+++ usuarios/class/CotizacionesEditar.php
@@ -148,7 +148,7 @@ class CotizacionesEditar {
 
             $htmlTabla .= '<tr class="combo">';
             $htmlTabla .= '<td>' . $no . '</td>';
-            $htmlTabla .= '<td><input type="number" title="czpp_orden" name="'.$prod['czpp_id'].'" value="'.$prod['czpp_orden'].'>" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
+            $htmlTabla .= '<td><input type="number" title="czpp_orden" name="'.$prod['czpp_id'].'" value="'.$prod['czpp_orden'].'" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
             $htmlTabla .= '<td>';
             $htmlTabla .= '<a href="#" class="delete-combo" data-id="'. $prod['combo_id'].'"><i class="icon-trash"></i></a>';
             $htmlTabla .= '<a href="combos-editar.php?id=' . $prod['combo_id'] . '" target="_blank">' . $prod['combo_nombre'] . '</a><br>';
@@ -191,6 +191,66 @@ class CotizacionesEditar {
                 $usuarioDctoEspecialAprobar = mysqli_fetch_array($consultaDctoEspecial, MYSQLI_BOTH);
                 $htmlTabla .= '<br><span style="font-size:10px; color:gray;">' . $prod['czpp_aprobado_fecha'] . '<br>' . $usuarioDctoEspecialAprobar['usr_nombre'] . '</span>';
                 $htmlTabla .= '</td>';
+            }
+            $htmlTabla .= '<td>'. '<span class="moneda-simbolo">' . $simbolosMonedas[$resultadoD['cotiz_moneda']].'</span>'. '	<span class="valor-numerico">'. number_format($valorTotal, 0, ",", ".") . '</span>'.'</td>';
+            $htmlTabla .= '</tr>';
+
+            $no++;
+        }
+
+        return "<body>$htmlTabla</body>"; // Devuelve el HTML de la tabla
+    }
+
+    public static function generarTablaServicios($conexionBdPrincipal, $resultadoD, $simbolosMonedas) {
+        $htmlTabla = ''; 
+        global $datosUsuarioActual;
+                            
+        $productos = $conexionBdPrincipal->query("SELECT * FROM servicios
+        INNER JOIN cotizacion_productos ON czpp_servicio=serv_id AND czpp_cotizacion='".$_GET["id"]."'
+        WHERE serv_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."'
+        ORDER BY czpp_orden");
+
+        $no = 1;
+        $totalIva = 0;
+        $subtotal = 0;
+        $totalDescuento = 0;
+        $totalCantidad = 0;
+        $sumaUtilidad = 0;
+
+        while ($prod = mysqli_fetch_array($productos, MYSQLI_BOTH)) {
+            $dcto = 0;
+            $valorTotal = 0;
+
+            $valorTotal = ($prod['czpp_valor'] * $prod['czpp_cantidad']);
+
+            if($prod['czpp_cantidad']>0 and $prod['czpp_descuento']>0){
+                $dcto = ($valorTotal * ($prod['czpp_descuento']/100));
+                $totalDescuento += $dcto;	
+            }
+
+            $valorConDcto = $valorTotal - $dcto;
+
+            $totalIva += ($valorConDcto * ($prod['czpp_impuesto']/100));
+
+            $subtotal +=$valorTotal;	
+            
+            
+            $totalCantidad += $prod['czpp_cantidad'];
+
+            $htmlTabla .= '<tr class="servicio">';
+            $htmlTabla .= '<td>' . $no . '</td>';
+            $htmlTabla .= '<td><input type="number" title="czpp_orden" name="'.$prod['czpp_id'].'" value="'.$prod['czpp_orden'].'" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
+            $htmlTabla .= '<td>';
+            $htmlTabla .= '<a href="#" class="delete-servicios" data-id="'. $prod['serv_id'].'"><i class="icon-trash"></i></a>';
+            $htmlTabla .= '<a href="servicios-editar.php?id=' . $prod['serv_id'] . '" target="_blank">' . $prod['serv_nombre'] . '</a><br>';
+            $htmlTabla .= '<p><textarea title="czpp_observacion" name="' . $prod['czpp_id'] . '" onChange="productos(this)" style="width: 300px;" rows="4">' . $prod['czpp_observacion'] . '</textarea></p>';
+            $htmlTabla .= '</td>';
+            $htmlTabla .= '<td><input type="number" title="czpp_cantidad" name="' . $prod['czpp_id'] . '" value="' . $prod['czpp_cantidad'] . '" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
+            $htmlTabla .= '<td><input type="text" alt="' . $resultadoD['cli_categoria'] . '" title="czpp_valor" name="' . $prod['czpp_id'] . '" value="' . $prod['czpp_valor'] . '" onChange="productos(this)" style="width: 200px;"></td>';
+            $htmlTabla .= '<td><input type="text" title="czpp_impuesto" name="' . $prod['czpp_id'] . '" value="' . $prod['czpp_impuesto'] . '" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
+            $htmlTabla .= '<td><input type="text" title="czpp_descuento" name="' . $prod['czpp_id'] . '" value="' . $prod['czpp_descuento'] . '" onChange="productos(this)" style="width: 50px; text-align: center;"></td>';
+            if ($resultadoD['cotiz_descuentos_especiales'] == 1) {
+                $htmlTabla .= '<td><input type="text" title="czpp_descuento_especial" name="' . $prod['czpp_id'] . '" value="' . $prod['czpp_descuento_especial'] . '" onChange="combos(this)" style="width: 50px; text-align: center;"></td>';
             }
             $htmlTabla .= '<td>'. '<span class="moneda-simbolo">' . $simbolosMonedas[$resultadoD['cotiz_moneda']].'</span>'. '	<span class="valor-numerico">'. number_format($valorTotal, 0, ",", ".") . '</span>'.'</td>';
             $htmlTabla .= '</tr>';
