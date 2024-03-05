760a56f1ed83abb5f8f6f7193d518341c22db683
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-03-05 16:58:11 -0500

    OR-90 - Se crean archivos ajax para servicios

---
+++ usuarios/ajax/ajax-buscar-servicio.php
@@ -0,0 +1,20 @@
+<?php
+include("../sesion.php");
+
+$search = $_GET["term"]; 
+
+$sql = "SELECT serv_id, serv_nombre FROM servicios WHERE serv_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."' AND serv_nombre LIKE '%$search%' ";
+$result = mysqli_query($conexionBdPrincipal, $sql);
+
+
+$results = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $results[] = [
+
+        "id" => $row["serv_id"],
+        "text" => strtoupper($row["serv_nombre"]),
+    ];
+}
+
+echo json_encode($results);
+?>
