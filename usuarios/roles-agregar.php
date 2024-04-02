7223b848d6f87fc03f8f9f0562bceac25f2e4c7f
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-04-02 16:17:30 -0500

    HTF - Se modifica la forma de agregar las paginas al crear un rol

--- usuarios/roles-agregar.php
+++ usuarios/roles-agregar.php
@@ -57,69 +57,40 @@ include("includes/js-formularios.php");
 							<h3> <?=$paginaActual['pag_nombre'];?></h3>
 						</div>
 						<div class="widget-container">
-							<form class="form-horizontal" method="post" action="bd_create/roles-guardar.php">
-                                
-                                <div class="control-group">
-									<label class="control-label">Nombre</label>
-									<div class="controls">
-										<input type="text" class="span6" name="nombre">
-									</div>
-								</div>
-                                
-								<table class="table table-striped table-bordered" id="data-table">
-										<thead>
-												<tr>
-														<th>ID</th>
-														<th>Nombre</th>
-														<th>Seleccionar <input type="checkbox" id="selectAll"></th>
-												</tr>
-										</thead>
-										<tbody>
-												<?php
-												$query = "SELECT p.pag_id, p.pag_nombre, pp.pper_id 
-														FROM paginas p 
-														LEFT JOIN ".MAINBD.".paginas_perfiles pp 
-														ON p.pag_id = pp.pper_pagina 
-														AND pp.pper_tipo_usuario = '" . $resultadoD['utipo_id'] . "'";
-												$result = $conexionBdAdmin->query($query);
-												$no=1;
-												while ($row = $result->fetch_assoc()) {
-														?>
-														<tr>
-																<td><?= $row['pag_id'];?></td>
-																<td><?= $row['pag_nombre']; ?></td>
-																<td>
-																		<input class="selectCheckbox" type="checkbox" value="<?= $row['pag_id']; ?>">
-																		<span style="display: none;"> <?= $isChecked; ?> </span>
-																</td>
-														</tr>
-												<?php
-												$no++;
-												}
-												?>
-										</tbody>
-								</table>
-                                <!--
-                                
-                                <span style="color:#F00;">Utiliza la siguiente opción (<b>Acciones NO permitidas</b>) sólo si NO utilizaste la opción anterior (<b>Paginas NO permitidas</b>) y viceversa.</span>
-                                
-                                <div class="control-group">
-									<label class="control-label">Acciones NO permitidas</label>
-									<div class="controls">
-										<select data-placeholder="Escoja una opción" class="chzn-select span6" multiple tabindex="4" name="accionesNP[]">
-											<option value=""></option>
-                                            <option value="2">Agregar</option>
-                                            <option value="3">Editar</option>
-                                            <option value="3">Eliminar</option>
-										</select>
-									</div>
-								</div>
 
-                            -->
-								<select id="paginasSeleccionadas"  name="paginasP[]" multiple  style="display: none;"></select>                               
+							<ul class="nav nav-tabs" id="myTab1">
+								<?php
+								$queryModulos = "SELECT * FROM modulos 
+								INNER JOIN modulos_empresa ON mxe_id_modulo=mod_id AND mxe_id_empresa='".$_SESSION["dataAdicional"]["id_empresa"]."' 
+								WHERE mod_padre IS NULL";
+								$resultModulos = $conexionBdAdmin->query($queryModulos);
+								$conta = 1;
+								while ($rowModulos = $resultModulos->fetch_assoc()) {
+								?>
+									<li <?php if($conta == 1) echo 'class="active"';?> id="mod<?=$rowModulos['mod_id'];?>"><a onclick="mostrarPaginas(<?=$rowModulos['mod_id'];?>, <?=$_GET['id'];?>)"><i class="icon-tasks"></i> <?=$rowModulos['mod_nombre'];?></a></li>
+								<?php
+									$conta ++;
+								}
+								?>
+							</ul>
+
+							<form class="form-horizontal" method="post" action="bd_create/roles-guardar.php">
+								<div class="tab-content">
+									<div class="control-group">
+										<label class="control-label">Nombre</label>
+										<div class="controls">
+											<input type="text" class="span4" name="nombre" id="nombre" value="<?=$resultadoD['utipo_nombre'];?>" required>
+										</div>
+									</div> 
+									
+									<div id="divTablePaginas"></div>
+								</div>
+											
+								<select id="paginasSeleccionadas"  name="paginasP[]" multiple  style="display: none;"></select>                 
+
 								<div class="form-actions">
+									<a href="javascript:history.go(-1);" class="btn btn-primary"><i class="icon-arrow-left"></i> Regresar</a>
 									<button type="submit" class="btn btn-info"><i class="icon-save"></i> Guardar cambios</button>
-									<button type="button" class="btn btn-danger">Cancelar</button>
 								</div>
 							</form>
 						</div>
@@ -132,42 +103,46 @@ include("includes/js-formularios.php");
 	<?php include("includes/pie.php");?>
 </div>
 </body>
+<script src="js/Roles.js" ></script>
 <script type="text/javascript">
-	let dataTable = $('#data-table').DataTable()
-	    function actualizarPaginasSeleccionadas(dataTable) {
-        let paginasSeleccionadas = [];
-				dataTable.$('.selectCheckbox').each(function() {
-            if ($(this).prop('checked')) {
-                paginasSeleccionadas.push($(this).val());
-            }
-        });
+	$(document).ready(mostrarPaginas(1, <?=$_GET['id'];?>));
+</script>
+<script type="text/javascript">
+	// let dataTable = $('#data-table').DataTable()
+	//     function actualizarPaginasSeleccionadas(dataTable) {
+    //     let paginasSeleccionadas = [];
+	// 			dataTable.$('.selectCheckbox').each(function() {
+    //         if ($(this).prop('checked')) {
+    //             paginasSeleccionadas.push($(this).val());
+    //         }
+    //     });
 
-				$('#selectAll').change(function() {
-				let isChecked = $(this).prop('checked');	
-				dataTable.$('.selectCheckbox').each(function() {
-								$(this).prop('checked',isChecked)
-                paginasSeleccionadas.push($(this).val());
-        });
-			});
+	// 			$('#selectAll').change(function() {
+	// 			let isChecked = $(this).prop('checked');	
+	// 			dataTable.$('.selectCheckbox').each(function() {
+	// 							$(this).prop('checked',isChecked)
+    //             paginasSeleccionadas.push($(this).val());
+    //     });
+	// 		});
 			
-				let selectElement = $('#paginasSeleccionadas');
-        selectElement.find('option').remove(); 
-				for (let i = 0; i < paginasSeleccionadas.length; i++) {
-						selectElement.append($('<option>', {
-								value: paginasSeleccionadas[i]
-						}));
-				}
-				selectElement.val(paginasSeleccionadas);
+	// 			let selectElement = $('#paginasSeleccionadas');
+    //     selectElement.find('option').remove(); 
+	// 			for (let i = 0; i < paginasSeleccionadas.length; i++) {
+	// 					selectElement.append($('<option>', {
+	// 							value: paginasSeleccionadas[i]
+	// 					}));
+	// 			}
+	// 			selectElement.val(paginasSeleccionadas);
 
-    }
+    // }
 
-    $(document).ready(function() {
-        actualizarPaginasSeleccionadas(dataTable);
+    // $(document).ready(function() {
+    //     actualizarPaginasSeleccionadas(dataTable);
 
-        $(document).on('change', 'input[type="checkbox"]', function() {
-            actualizarPaginasSeleccionadas(dataTable);
-        });
-    });
+    //     $(document).on('change', 'input[type="checkbox"]', function() {
+    //         actualizarPaginasSeleccionadas(dataTable);
+    //     });
+    // });
 		
 </script>
 </html>
