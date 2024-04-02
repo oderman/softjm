8432c4aae9377e983a3cd60ae55dbbedc4028689
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-04-02 09:16:12 -0500

    HTF - Se corrige detalle al esacojer los modulos

--- usuarios/bd_update/actualizar-cliente-orion.php
+++ usuarios/bd_update/actualizar-cliente-orion.php
@@ -13,21 +13,6 @@
 
 	$conexionBdAdmin->query("UPDATE clientes_orion SET  clio_empresa='" . $_POST["nombre"] . "', clio_email='" . $_POST["email"] . "', clio_telefono='" . $_POST["telefono"] . "', clio_contacto_principal='" . $_POST["contacto"] . "', clio_fecha_inicio='" . $_POST["inicio"] . "', clio_fecha_fin='" . $_POST["fin"] . "' WHERE clio_id='" . $_POST["id"] . "'");
 
-    if(isset($_POST["modulo"])){
-        $conexionBdAdmin->query("DELETE FROM modulos_empresa WHERE mxe_id_empresa='" . $_POST["id"] . "'");
-    
-        $numero = (count($_POST["modulo"]));
-        $contador = 0;        
-        while ($contador < $numero) {
-    
-            $conexionBdAdmin->query("INSERT INTO modulos_empresa(mxe_id_modulo, mxe_id_empresa)VALUES('" . $_POST["modulo"][$contador] . "','" . $_POST["id"] . "')");
-            
-            $contador++;
-        }
-    }else{
-        $conexionBdAdmin->query("DELETE FROM modulos_empresa WHERE mxe_id_empresa='" . $_POST["id"] . "'");
-    } 
-
 	echo '<script type="text/javascript">window.location.href="../clientes-orion-editar.php?id=' . $_POST["id"] . '&msg=2";</script>';
 
     include(RUTA_PROYECTO."/usuarios/includes/guardar-historial-acciones.php");
