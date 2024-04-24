3f3318821b680851b8b3041131c097544751e9d0
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-04-24 15:30:56 -0500

    HTF - Mejora para caracteres especiales

--- conexion.php
+++ conexion.php
@@ -2,5 +2,14 @@
 require_once("constantes.php");
 
 $conexionBdPrincipal = new mysqli(SERVER, USER, PASS, MAINBD);
+if (!$conexionBdPrincipal->set_charset("utf8mb4")) {
+    die("Error al establecer el conjunto de caracteres: " . $conexionBdPrincipal->error);
+}
 $conexionBdAdmin     = new mysqli(SERVER, USER, PASS, BDADMIN);
+if (!$conexionBdAdmin->set_charset("utf8mb4")) {
+    die("Error al establecer el conjunto de caracteres: " . $conexionBdAdmin->error);
+}
 $conexionBdStore     = new mysqli(SERVER, USER, PASS, BDSTORE);
+if (!$conexionBdStore->set_charset("utf8mb4")) {
+    die("Error al establecer el conjunto de caracteres: " . $conexionBdStore->error);
+}
